5f1dd17c7e57179588441ee2e3d6a4241f03aa95
W-46ec <30034417+W-46ec@users.noreply.github.com>
2020-04-03 06:04:39 +0800

    30-Day - Day01

---
+++ 30-Day LeetCoding Challenge_2020 April/01_singleNumber.py
@@ -0,0 +1,37 @@
+
+"""
+Single Number
+
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+Input: [2,2,1]
+Output: 1
+
+Example 2:
+Input: [4,1,2,1,2]
+Output: 4
+
+"""
+
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # i, nums = 0, sorted(nums)
+        # while i < len(nums) - 1:
+        #     if nums[i] == nums[i + 1]:
+        #         i += 2
+        #     else:
+        #         return nums[i]
+        # return nums[-1]
+        
+        from functools import reduce
+        return reduce(lambda x, y: x ^ y, nums)
+
+print(Solution().singleNumber([2, 2, 1]))           # 1
+print(Solution().singleNumber([4, 1, 2, 1, 2]))     # 4
