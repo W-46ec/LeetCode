063bd7e4a819dd8cf8785ed99a7663f44ef8ba17
W-46ec <30034417+W-46ec@users.noreply.github.com>
2020-06-15 17:19:41 +0800

    June 14 - img

--- 30-Day LeetCoding Challenge/2020_06/14_Cheapest Flights Within K Stops.py
+++ 30-Day LeetCoding Challenge/2020_06/14_Cheapest Flights Within K Stops.py
@@ -14,7 +14,7 @@ src = 0, dst = 2, k = 1
 Output: 200
 Explanation: 
 The graph looks like this:
-
+![14_995](./img/14_995.png)
 
 The cheapest price from city 0 to city 2 with at most 1 stop costs 200, as marked red in the picture.
 ```
@@ -27,7 +27,7 @@ src = 0, dst = 2, k = 0
 Output: 500
 Explanation: 
 The graph looks like this:
-
+![14_995](./img/14_995.png)
 
 The cheapest price from city 0 to city 2 with at most 0 stop costs 500, as marked blue in the picture.
 ```
