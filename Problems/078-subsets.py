56b9cffb336834f7f632462cdadd5cb313e3e8b7
W-46ec <30034417+W-46ec@users.noreply.github.com>
2020-07-12 05:19:15 +0800

    Question 078

--- Problems/078-subsets.py
+++ Problems/078-subsets.py
@@ -1,27 +1,35 @@
+
+"""
+# Subsets
+
+Given a set of **distinct** integers, *nums*, return all possible subsets (the power set).
+
+**Note:** The solution set must not contain duplicate subsets.
+
+**Example:** 
+```
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+```
+"""
+
+from typing import List
+from itertools import combinations
+from functools import reduce
+
 class Solution:
-	def subsets(self, nums):
-		"""
-		:type nums: List[int]
-		:rtype: List[List[int]]
-		"""
-		ans = []
-		def combination(U, subset, targetLength):
-			if len(subset) == targetLength:
-				ans.append(subset)
-			elif len(subset) > targetLength:
-				return
-			else:
-				if len(subset) != 0:
-					idx = U.index(subset[-1])
-				else:
-					idx = -1
-				for i in range(idx + 1, len(U)):
-					combination(U, subset + [U[i]], targetLength)
-		for i in range(len(nums) + 1):
-			if i == 0:
-				ans.append([])
-				continue
-			combination(nums, [], i)
-		return ans
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        return reduce(lambda x, y: x + y, [[list(x) for x in list(combinations(nums, i))] for i in range(len(nums) + 1)])
 
 print(Solution().subsets([1, 2, 3]))
+
