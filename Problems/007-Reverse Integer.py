53b8b38216440fceece171497534b8a273be4026
W-46ec <30034417+W-46ec@users.noreply.github.com>
2020-07-14 05:58:35 +0800

    Question 007 - Optimization

--- Problems/007-Reverse Integer.py
+++ Problems/007-Reverse Integer.py
@@ -1,26 +1,41 @@
 
-def reverse(x):
-	"""
-	:type x: int
-	:rtype: int
-	"""
+"""
+# Reverse Integer
 
-	sign = 1
-	if x < 0:
-		sign *= -1
-		x *= sign
-	elif x == 0:
-		return 0
+Given a 32-bit signed integer, reverse digits of an integer.
 
-	l = list(str(x))
-	l.reverse()
+**Example 1:** 
+```
+Input: 123
+Output: 321
+```
 
-	while l[0] == '0':
-		l.pop(0)
-		
-	x = int("".join(l)) * sign
-	if x > 2 ** 31 - 1 or x < -(2 ** 31):
-		return 0
-	return x
+**Example 2:** 
+```
+Input: -123
+Output: -321
+```
 
-print(reverse(1534236469))
+**Example 3:** 
+```
+Input: 120
+Output: 21
+```
+
+**Note:** 
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+"""
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign, x, ans = 1 if x >= 0 else -1, abs(x), 0
+        while x:
+            ans = 10 * ans + x % 10
+            x //= 10
+        return sign * ans if ans >= -(2 ** 31) and ans < 2 ** 31 else 0
+
+print(Solution().reverse(123))          # 321
+print(Solution().reverse(-123))         # -321
+print(Solution().reverse(120))          # 21
+print(Solution().reverse(1534236469))   # 0
+
