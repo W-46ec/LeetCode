4ad12cb5c875f12539b4948f9d8fba849c9373ef
W-46ec <30034417+W-46ec@users.noreply.github.com>
2020-07-15 07:51:31 +0800

    Question 008

---
+++ Problems/008-String to Integer (atoi).py
@@ -0,0 +1,83 @@
+
+"""
+# String to Integer (atoi)
+
+Implement `atoi` which converts a string to an integer.
+
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+
+The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+
+If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned.
+
+**Note:** 
+    - Only the space character `' '` is considered as whitespace character.
+    - Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. If the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
+
+**Example 1:** 
+```
+Input: "42"
+Output: 42
+```
+
+**Example 2:** 
+```
+Input: "   -42"
+Output: -42
+Explanation: The first non-whitespace character is '-', which is the minus sign.
+             Then take as many numerical digits as possible, which gets 42.
+```
+
+**Example 3:** 
+```
+Input: "4193 with words"
+Output: 4193
+Explanation: Conversion stops at digit '3' as the next character is not a numerical digit.
+```
+
+**Example 4:** 
+```
+Input: "words and 987"
+Output: 0
+Explanation: The first non-whitespace character is 'w', which is not a numerical 
+             digit or a +/- sign. Therefore no valid conversion could be performed.
+```
+
+**Example 5:** 
+```
+Input: "-91283472332"
+Output: -2147483648
+Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
+             Thefore INT_MIN (−231) is returned.
+```
+"""
+
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        ans, lst = 0, list(filter(lambda x: len(x) > 0, str.strip().split(' ')))
+        if len(lst) > 0:
+            idx = 1 if lst[0][0] == '-' or lst[0][0] == '+' else 0
+            while idx < len(lst[0]) and lst[0][idx].isnumeric():
+                idx += 1
+            w = lst[0][ : idx]
+            ans = int(w) if idx > 0 and w != '-' and w != '+' else 0
+        if ans >= 2 ** 31:
+            return 2 ** 31 - 1
+        elif ans < -(2 ** 31):
+            return -(2 ** 31)
+        else:
+            return ans
+
+print(Solution().myAtoi("42"))                  # 42
+print(Solution().myAtoi("   -42"))              # -42
+print(Solution().myAtoi("4193 with words"))     # 4193
+print(Solution().myAtoi("words and 987"))       # 0
+print(Solution().myAtoi("91283472332"))         # 2147483648
+print(Solution().myAtoi("   -23 44"))           # -23
+print(Solution().myAtoi("3.14159"))             # 3
+print(Solution().myAtoi("+1"))                  # 1
+print(Solution().myAtoi("+"))                   # 0
+print(Solution().myAtoi("2147483648"))          # 2147483647
+
