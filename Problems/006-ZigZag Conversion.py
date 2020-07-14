954f8c431efca1023ffc4447a067c2356d8ce8ec
W-46ec <30034417+W-46ec@users.noreply.github.com>
2020-07-15 03:02:33 +0800

    Question 006 - Optimization

--- Problems/006-ZigZag Conversion.py
+++ Problems/006-ZigZag Conversion.py
@@ -1,34 +1,51 @@
-def convert(s, numRows):
-	"""
-	:type s: str
-	:type numRows: int
-	:rtype: str
-	"""
-	
-	if numRows == 1:
-		return s
 
-	converted = []
+"""
+# ZigZag Conversion
 
-	for i in range(0, numRows):
-		converted.append([])
+The string `"PAYPALISHIRING"` is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+```
+P   A   H   N
+A P L S I I G
+Y   I   R
+```
 
-	direction = 'down'
-	j = -1
-	for i in s:
-		if direction == 'down':
-			j += 1
-			converted[j].append(i)
-			if j == numRows - 1:
-				direction = 'up'
-		elif direction == 'up':
-			j -= 1
-			converted[j].append(i)
-			if j == 0:
-				direction = 'down'
-	string = ''
-	for i in converted:
-		string += "".join(i)
-	return string
+And then read line by line: `"PAHNAPLSIIGYIR"` 
 
-print(convert("1234567890", 3))
+Write the code that will take a string and make this conversion given a number of rows:
+```
+string convert(string s, int numRows);
+```
+
+**Example 1:** 
+```
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+```
+
+**Example 2:** 
+```
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+```
+"""
+
+from functools import reduce
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        lst, order = [[] for i in range(numRows)], [i for i in range(numRows)]
+        order += order[-2 : 0 : -1]
+        for i, c in enumerate(s):
+            lst[order[i % len(order)]].append(c)
+        return reduce(lambda x, y: x + y, ["".join(x) for x in lst])
+
+
+print(Solution().convert("PAYPALISHIRING", 3))  # "PAHNAPLSIIGYIR"
+print(Solution().convert("PAYPALISHIRING", 4))  # "PINALSIGYAHRPI"
+
