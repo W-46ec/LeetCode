0b37066678c573576e03a6f32151184741ddfeb5
W-46ec <30034417+W-46ec@users.noreply.github.com>
2020-08-13 14:39:58 +0800

    August 12

---
+++ 30-Day LeetCoding Challenge/2020_08/12_Pascal's Triangle II.py
@@ -0,0 +1,36 @@
+
+"""
+# Pascal's Triangle II
+
+Given a non-negative index *k* where *k* ≤ 33, return the *k*^th index row of the Pascal's triangle.
+
+Note that the row index starts from 0.
+
+![12_PascalTriangleAnimated2](./img/12_PascalTriangleAnimated2.gif)
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+**Example:** 
+```
+Input: 3
+Output: [1,3,3,1]
+```
+
+**Follow up:** 
+Could you optimize your algorithm to use only *O(k)* extra space?
+"""
+
+from typing import List
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        ans = []
+        while rowIndex + 1:
+            for j in range(len(ans) - 1):
+                ans[j] = ans[j] + ans[j + 1]
+            ans = [1] + ans
+            rowIndex -= 1
+        return ans
+
+print(Solution().getRow(3))     # [1, 3, 3, 1]
+
