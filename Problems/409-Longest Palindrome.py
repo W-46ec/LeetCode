30cf1fcec33d96561637951d54c415fbd2b53f73
W-46ec <30034417+W-46ec@users.noreply.github.com>
2020-08-15 13:48:29 +0800

    Question 409

---
+++ Problems/409-Longest Palindrome.py
@@ -0,0 +1,39 @@
+
+"""
+# Longest Palindrome
+
+Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+
+This is case sensitive, for example `"Aa"` is not considered a palindrome here.
+
+**Note:** 
+Assume the length of given string will not exceed 1,010.
+
+**Example:** 
+```
+Input:
+"abccccdd"
+
+Output:
+7
+
+Explanation:
+One longest palindrome that can be built is "dccaccd", whose length is 7.
+```
+"""
+
+from collections import Counter
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        ans, freq = 0, Counter(s)
+        for key in freq:
+            ans += freq[key] // 2 * 2 + (1 if not ans % 2 and freq[key] % 2 else 0)
+        return ans
+
+# 7
+print(Solution().longestPalindrome("abccccdd"))
+
+# 983
+print(Solution().longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
+
