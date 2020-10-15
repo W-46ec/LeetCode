f998e5d7690a474a5fc55a7bf16f169d9d1acfb6
W-46ec <30034417+W-46ec@users.noreply.github.com>
2020-10-15 16:15:01 +0800

    October 13 - Images

--- 30-Day LeetCoding Challenge/2020_10/13_Sort List.py
+++ 30-Day LeetCoding Challenge/2020_10/13_Sort List.py
@@ -8,14 +8,14 @@ Given the `head` of a linked list, return *the list after sorting it in **ascend
 
 
 **Example 1:** 
-
+![13_sort_list_1](./img/13_sort_list_1.jpg)
 ```
 Input: head = [4,2,1,3]
 Output: [1,2,3,4]
 ```
 
 **Example 2:** 
-
+![13_sort_list_2](./img/13_sort_list_2.jpg)
 ```
 Input: head = [-1,5,3,4,0]
 Output: [-1,0,3,4,5]
