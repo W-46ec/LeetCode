6cf1f15277be8f415a97d36758ee85048f870178
W-46ec <30034417+W-46ec@users.noreply.github.com>
2020-10-17 08:38:19 +0800

    Question 035 - Minor change

--- Problems/035-Search Insert Position.py
+++ Problems/035-Search Insert Position.py
@@ -2,52 +2,62 @@
 """
 # Search Insert Position
 
-Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
-You may assume no duplicates in the array.
 
 **Example 1:** 
 ```
-Input: [1,3,5,6], 5
+Input: nums = [1,3,5,6], target = 5
 Output: 2
 ```
 
 **Example 2:** 
 ```
-Input: [1,3,5,6], 2
+Input: nums = [1,3,5,6], target = 2
 Output: 1
 ```
 
 **Example 3:** 
 ```
-Input: [1,3,5,6], 7
+Input: nums = [1,3,5,6], target = 7
 Output: 4
 ```
 
 **Example 4:** 
 ```
-Input: [1,3,5,6], 0
+Input: nums = [1,3,5,6], target = 0
 Output: 0
 ```
+
+**Example 5:** 
+```
+Input: nums = [1], target = 0
+Output: 0
+```
+
+**Constraints:** 
+    - `1 <= nums.length <= 10^4` 
+    - `-10^4 <= nums[i] <= 10^4` 
+    - `nums` contains **distinct** values sorted in **ascending** order.
+    - `-10^4 <= target <= 10^4` 
 """
 
 from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        i, j = 0, len(nums) - 1
-        while i <= j:
-            mid = (i + j) // 2
+        lo, hi = 0, len(nums) - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
             if nums[mid] == target:
                 return mid
-            if target > nums[mid]:
-                i = mid + 1
-            else:
-                j = mid - 1
-        return i
+            lo = mid + 1 if target > nums[mid] else lo
+            hi = mid - 1 if target < nums[mid] else hi
+        return lo
 
-print(Solution().searchInsert([1, 3, 5, 6], 5))
-print(Solution().searchInsert([1, 3, 5, 6], 2))
-print(Solution().searchInsert([1, 3, 5, 6], 7))
-print(Solution().searchInsert([1, 3, 5, 6], 0))
+print(Solution().searchInsert([1, 3, 5, 6], 5))     # 2
+print(Solution().searchInsert([1, 3, 5, 6], 2))     # 1
+print(Solution().searchInsert([1, 3, 5, 6], 7))     # 4
+print(Solution().searchInsert([1, 3, 5, 6], 0))     # 0
+print(Solution().searchInsert([1], 0))              # 0
 
