da6ba0b16f80a3bb73d6a36e8adeddfe81caefb6
W-46ec <30034417+W-46ec@users.noreply.github.com>
2021-02-13 20:11:43 -0800

    Question 881

---
+++ Problems/881-Boats to Save People.py
@@ -0,0 +1,54 @@
+
+"""
+# You are given an array `people` where `people[i]` is the weight of the `ith` person, and an **infinite number of boats** where each boat can carry a maximum weight of `limit`. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most `limit`.
+
+Return *the minimum number of boats to carry every given person*.
+
+
+**Example 1:** 
+```
+Input: people = [1,2], limit = 3
+Output: 1
+Explanation: 1 boat (1, 2)
+```
+
+**Example 2:** 
+```
+Input: people = [3,2,2,1], limit = 3
+Output: 3
+Explanation: 3 boats (1, 2), (2) and (3)
+```
+
+**Example 3:** 
+```
+Input: people = [3,5,3,4], limit = 5
+Output: 4
+Explanation: 4 boats (3), (3), (4), (5)
+```
+
+**Constraints:** 
+    - `1 <= people.length <= 5 * 10^4` 
+    - `1 <= people[i] <= limit <= 3 * 10^4` 
+"""
+
+from typing import List
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people, ans, lo, hi = sorted(people), 0, 0, len(people) - 1
+        while lo <= hi:
+            ans += 1
+            if people[lo] + people[hi] <= limit:
+                lo += 1
+            hi -= 1
+        return ans
+
+# 1
+print(Solution().numRescueBoats([1, 2], 3))
+
+# 3
+print(Solution().numRescueBoats([3, 2, 2, 1], 3))
+
+# 4
+print(Solution().numRescueBoats([3, 5, 3, 4], 5))
+
