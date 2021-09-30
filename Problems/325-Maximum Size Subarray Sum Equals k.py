550a9d8f398bd4cce98932d5f92872a35b3adffd
W-46ec <30034417+W-46ec@users.noreply.github.com>
2021-09-30 14:09:05 -0700

    Question 325 - Optimization

--- Problems/325-Maximum Size Subarray Sum Equals k.py
+++ Problems/325-Maximum Size Subarray Sum Equals k.py
@@ -38,28 +38,28 @@ from typing import List
 
 class Solution:
     def maxSubArrayLen(self, nums: List[int], k: int) -> int:
-        # Contruct a prefix sum array such that prefixSum[i]
-        # is the sum of the first i elements in nums
-        prefixSum, S_i = [], 0
-        for x in nums:
-            S_i += x
-            prefixSum.append(S_i)
-        
-        # Now the problem is similar to the Two Sum problem:
-        # Find indices i, j such that prefixSum[j] - prefixSum[i] is k
+        # Assume we have a prefix sum array such that 
+        # prefixSum[i] is the sum of the first i elements in nums.
+        # Then the problem is similar to the Two Sum problem:
+        # Find indices i, j such that prefixSum[j] - prefixSum[i] is k.
+        # We don't  actually have to store the whole prefixSum array.
+        # We can compute the needed element while iterating through nums.
         indicesMap = {}
-        i, j = 0, -1
-        for idx, val in enumerate(prefixSum):
+        i, j, prefixSum_i = 0, -1, 0
+        for idx in range(len(nums)):
+
+            # Calculate the ith value of prefixSum[]
+            prefixSum_i += nums[idx]
             
             # If prefixSum[idx] is k, then nums[0 : idx + 1] is the
             # longest subarray
-            if val == k:
+            if prefixSum_i == k:
                 i, j = 0, idx
             
             # We want to check if we have previously seen a prefixSum[i]
             # such that prefixSum[idx] - prefixSum[i] == k
             # i.e., prefixSum[i] == prefixSum[idx] - k
-            wanted = prefixSum[idx] - k
+            wanted = prefixSum_i - k
             if wanted in indicesMap:
                 # We have seen such prefixSum[i] before
                 if idx - indicesMap[wanted] + 1 > j - i + 1:
@@ -67,14 +67,14 @@ class Solution:
                     # is sum(nums[i + 1], nums[i + 2], ..., nums[j])
                     i, j = indicesMap[wanted] + 1, idx
             
-            # Store the current value in the hashmap so that
+            # Store the current sum in the hashmap so that
             # if it is what we need in the subsequent steps,
             # we can access it in O(1) time.
             # Note that if the same value has been seen before, we
             # should keep the smaller index so that the resulting
             # subarray is the longest
-            if val not in indicesMap:
-                indicesMap[val] = idx
+            if prefixSum_i not in indicesMap:
+                indicesMap[prefixSum_i] = idx
 
         return j - i + 1
 
