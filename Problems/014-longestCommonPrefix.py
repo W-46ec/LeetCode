d23c86ff6aee0f83a19d2102863014038d67caa5
W-46ec <30034417+W-46ec@users.noreply.github.com>
2021-10-06 23:12:32 -0700

    Question 014 - Optimization

--- Problems/014-longestCommonPrefix.py
+++ Problems/014-longestCommonPrefix.py
@@ -1,31 +1,45 @@
-def longestCommonPrefix(strs):
-	"""
-	:type strs: List[str]
-	:rtype: str
-	"""
 
-	prefix = ''
-	shortest = ''
+"""
+# Longest Common Prefix
 
-	if len(strs) > 0:
-		shortest = strs[0]
-	else:
-		return ''
-	for e in strs:
-		if len(e) < len(shortest):
-			shortest = e
+Write a function to find the longest common prefix string amongst an array of strings.
 
-	for i in range(0, len(shortest)):
-		commonPrefix = True
-		for c in strs:
-			if c[i] != shortest[i]:
-				commonPrefix = False
-		if commonPrefix:
-			prefix += shortest[i]
-		else:
-			break
-
-	return prefix
+If there is no common prefix, return an empty string `""`.
 
 
-print(longestCommonPrefix(['aca', 'cba']))
+**Example 1:** 
+```
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+```
+
+**Example 2:** 
+```
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+```
+
+**Constraints:** 
+    - `1 <= strs.length <= 200` 
+    - `0 <= strs[i].length <= 200` 
+    - `strs[i]` consists of only lower-case English letters.
+"""
+
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        ans = ''
+        for prefix in zip(*map(list, strs)):
+            if all([x == prefix[0] for x in prefix]):
+                ans += prefix[0]
+            else:
+                break
+        return ans
+
+# "fl"
+print(Solution().longestCommonPrefix(["flower", "flow", "flight"]))
+
+# ""
+print(Solution().longestCommonPrefix(["dog", "racecar", "car"]))
