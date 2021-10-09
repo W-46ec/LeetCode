6d04b1b0a4d5bf4c0077bdf6052bd18e0e6aa9a3
W-46ec <30034417+W-46ec@users.noreply.github.com>
2021-10-09 13:55:53 -0700

    Question 212 - Further optimization

--- Problems/212-Word Search II.py
+++ Problems/212-Word Search II.py
@@ -42,22 +42,28 @@ If the current candidate does not exist in all words' prefix, you could stop bac
 
 from typing import List
 from collections import defaultdict
+from functools import reduce
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        m, n = len(board), len(board[0])
+        m, n, ans = len(board), len(board[0]), []
         visited = [[False] * n for _ in range(m)]
-        ans = []
+
+        # Set of all characters on the board
+        char_set = reduce(lambda x, y: x | y, map(set, board))
 
         # Build a prefix tree
         prefix_tree = {}
         for word in words:
-            tree = prefix_tree
-            for c in word:
-                if c not in tree:
-                    tree[c] = {}
-                tree = tree[c]
-            tree['\0'] = {}
+            # Only insert words whose initial 
+            # character is on the board
+            if word[0] in char_set:
+                tree = prefix_tree
+                for c in word:
+                    if c not in tree:
+                        tree[c] = {}
+                    tree = tree[c]
+                tree['\0'] = {}
 
         # Define DFS
         def dfs(x, y, tree, prefix):
@@ -147,4 +153,20 @@ print(Solution().findWords([
     ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'], 
     ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a']
 ], ['a', 'aa', 'aaa', 'aaaa', 'aaaaa', 'aaaaaa', 'aaaaaaa', 'aaaaaaaa', 'aaaaaaaaa', 'aaaaaaaaaa']))
+
+# ['a', 'aa', 'aaa', 'aaaa', 'aaaaa', 'aaaaaa', 'aaaaaaa', 'aaaaaaaa', 'aaaaaaaaa', 'aaaaaaaaaa']
+print(Solution().findWords([
+    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'], 
+    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'], 
+    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'], 
+    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'], 
+    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'], 
+    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'], 
+    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'], 
+    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'], 
+    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'], 
+    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'], 
+    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'], 
+    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a']
+], ['lllllll', 'fffffff', 'ssss', 's', 'rr', 'xxxx', 'ttt', 'eee', 'ppppppp', 'iiiiiiiii', 'xxxxxxxxxx', 'pppppp', 'xxxxxx', 'yy', 'jj', 'ccc', 'zzz', 'ffffffff', 'r', 'mmmmmmmmm', 'tttttttt', 'mm', 'ttttt', 'qqqqqqqqqq', 'z', 'aaaaaaaa', 'nnnnnnnnn', 'v', 'g', 'ddddddd', 'eeeeeeeee', 'aaaaaaa', 'ee', 'n', 'kkkkkkkkk', 'ff', 'qq', 'vvvvv', 'kkkk', 'e', 'nnn', 'ooo', 'kkkkk', 'o', 'ooooooo', 'jjj', 'lll', 'ssssssss', 'mmmm', 'qqqqq', 'gggggg', 'rrrrrrrrrr', 'iiii', 'bbbbbbbbb', 'aaaaaa', 'hhhh', 'qqq', 'zzzzzzzzz', 'xxxxxxxxx', 'ww', 'iiiiiii', 'pp', 'vvvvvvvvvv', 'eeeee', 'nnnnnnn', 'nnnnnn', 'nn', 'nnnnnnnn', 'wwwwwwww', 'vvvvvvvv', 'fffffffff', 'aaa', 'p', 'ddd', 'ppppppppp', 'fffff', 'aaaaaaaaa', 'oooooooo', 'jjjj', 'xxx', 'zz', 'hhhhh', 'uuuuu', 'f', 'ddddddddd', 'zzzzzz', 'cccccc', 'kkkkkk', 'bbbbbbbb', 'hhhhhhhhhh', 'uuuuuuu', 'cccccccccc', 'jjjjj', 'gg', 'ppp', 'ccccccccc', 'rrrrrr', 'c', 'cccccccc', 'yyyyy', 'uuuu', 'jjjjjjjj', 'bb', 'hhh', 'l', 'u', 'yyyyyy', 'vvv', 'mmm', 'ffffff', 'eeeeeee', 'qqqqqqq', 'zzzzzzzzzz', 'ggg', 'zzzzzzz', 'dddddddddd', 'jjjjjjj', 'bbbbb', 'ttttttt', 'dddddddd', 'wwwwwww', 'vvvvvv', 'iii', 'ttttttttt', 'ggggggg', 'xx', 'oooooo', 'cc', 'rrrr', 'qqqq', 'sssssss', 'oooo', 'lllllllll', 'ii', 'tttttttttt', 'uuuuuu', 'kkkkkkkk', 'wwwwwwwwww', 'pppppppppp', 'uuuuuuuu', 'yyyyyyy', 'cccc', 'ggggg', 'ddddd', 'llllllllll', 'tttt', 'pppppppp', 'rrrrrrr', 'nnnn', 'x', 'yyy', 'iiiiiiiiii', 'iiiiii', 'llll', 'nnnnnnnnnn', 'aaaaaaaaaa', 'eeeeeeeeee', 'm', 'uuu', 'rrrrrrrr', 'h', 'b', 'vvvvvvv', 'll', 'vv', 'mmmmmmm', 'zzzzz', 'uu', 'ccccccc', 'xxxxxxx', 'ss', 'eeeeeeee', 'llllllll', 'eeee', 'y', 'ppppp', 'qqqqqq', 'mmmmmm', 'gggg', 'yyyyyyyyy', 'jjjjjj', 'rrrrr', 'a', 'bbbb', 'ssssss', 'sss', 'ooooo', 'ffffffffff', 'kkk', 'xxxxxxxx', 'wwwwwwwww', 'w', 'iiiiiiii', 'ffff', 'dddddd', 'bbbbbb', 'uuuuuuuuu', 'kkkkkkk', 'gggggggggg', 'qqqqqqqq', 'vvvvvvvvv', 'bbbbbbbbbb', 'nnnnn', 'tt', 'wwww', 'iiiii', 'hhhhhhh', 'zzzzzzzz', 'ssssssssss', 'j', 'fff', 'bbbbbbb', 'aaaa', 'mmmmmmmmmm', 'jjjjjjjjjj', 'sssss', 'yyyyyyyy', 'hh', 'q', 'rrrrrrrrr', 'mmmmmmmm', 'wwwww', 'www', 'rrr', 'lllll', 'uuuuuuuuuu', 'oo', 'jjjjjjjjj', 'dddd', 'pppp', 'hhhhhhhhh', 'kk', 'gggggggg', 'xxxxx', 'vvvv', 'd', 'qqqqqqqqq', 'dd', 'ggggggggg', 't', 'yyyy', 'bbb', 'yyyyyyyyyy', 'tttttt', 'ccccc', 'aa', 'eeeeee', 'llllll', 'kkkkkkkkkk', 'sssssssss', 'i', 'hhhhhh', 'oooooooooo', 'wwwwww', 'ooooooooo', 'zzzz', 'k', 'hhhhhhhh', 'aaaaa', 'mmmmm']))
 
