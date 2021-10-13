8ee837f446e80ce717d1de6ad1c24718be00dc18
W-46ec <30034417+W-46ec@users.noreply.github.com>
2021-10-12 19:21:06 -0700

    Question 217

---
+++ Problems/00217-Contains Duplicate.py
@@ -0,0 +1,43 @@
+
+"""
+# Contains Duplicate
+
+Given an integer array `nums`, return `true` if any value appears **at least twice** in the array, and return `false` if every element is distinct.
+
+**Example 1:** 
+```
+Input: nums = [1,2,3,1]
+Output: true
+```
+
+**Example 2:** 
+```
+Input: nums = [1,2,3,4]
+Output: false
+```
+
+**Example 3:** 
+```
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+```
+
+**Constraints:** 
+    - `1 <= nums.length <= 10^5` 
+    - `-10^9 <= nums[i] <= 10^9` 
+"""
+
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) < len(nums)
+
+# True
+print(Solution().containsDuplicate([1, 2, 3, 1]))
+
+# False
+print(Solution().containsDuplicate([1, 2, 3, 4]))
+
+# True
+print(Solution().containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
