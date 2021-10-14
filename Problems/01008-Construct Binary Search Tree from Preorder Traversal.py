6aceab8214091a6725c80b14655f70bad924c774
W-46ec <30034417+W-46ec@users.noreply.github.com>
2021-10-14 13:44:11 -0700

    Question 1008

--- Problems/01008-Construct Binary Search Tree from Preorder Traversal.py
+++ Problems/01008-Construct Binary Search Tree from Preorder Traversal.py
@@ -2,33 +2,46 @@
 """
 # Construct Binary Search Tree from Preorder Traversal
 
-Return the root node of a binary **search** tree that matches the given `preorder` traversal.
+Given an array of integers preorder, which represents the **preorder traversal** of a BST (i.e., **binary search tree**), construct the tree and return *its root*.
 
-*(Recall that a binary search tree is a binary tree where for every node, any descendant of `node.left` has a value `< node.val`, and any descendant of `node.right` has a value `> node.val`.  Also recall that a preorder traversal displays the value of the `node` first, then traverses `node.left`, then traverses `node.right`.)*
+It is **guaranteed** that there is always possible to find a binary search tree with the given requirements for the given test cases.
 
-It's guaranteed that for the given test cases there is always possible to find a binary search tree with the given requirements.
+A **binary search tree** is a binary tree where for every node, any descendant of `Node.left` has a value **strictly less than** `Node.val`, and any descendant of `Node.right` has a value **strictly greater than** `Node.val`.
+
+A **preorder traversal** of a binary tree displays the value of the node first, then traverses `Node.left`, then traverses `Node.right`.
+
 
 **Example 1:** 
+![1008_1266](./img/1008_1266.png)
 ```
-Input: [8,5,1,7,10,12]
+Input: preorder = [8,5,1,7,10,12]
 Output: [8,5,10,1,7,null,12]
-![24_1266](./img/1008_1266.png)
+```
+
+**Example 2:** 
+```
+Input: preorder = [1,3]
+Output: [1,null,3]
 ```
 
 **Constraints:** 
     - `1 <= preorder.length <= 100` 
     - `1 <= preorder[i] <= 10^8` 
-    - The values of `preorder` are distinct.
+    - All the values of preorder are **unique**.
 """
 
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+import sys
+sys.path  = sys.path = ['.', '../', '../../'] + sys.path
 
 from typing import List
+from util import TreeNode, serializeTree
 
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
@@ -45,4 +58,12 @@ class Solution:
             root = insert(root, i)
         return root
 
+# "[8, 5, 10, 1, 7, null, 12]"
+print(serializeTree(Solution().bstFromPreorder(
+    [8, 5, 1, 7, 10, 12]
+)))
 
+# "[1, null, 3]"
+print(serializeTree(Solution().bstFromPreorder(
+    [1, 3]
+)))
