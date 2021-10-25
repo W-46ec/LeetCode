6e9491bd5cb3484ef00424685207e908aee4425f
W-46ec <30034417+W-46ec@users.noreply.github.com>
2021-10-25 14:48:18 -0700

    Question 993 - Test

--- Problems/00993-Cousins in Binary Tree.py
+++ Problems/00993-Cousins in Binary Tree.py
@@ -2,47 +2,52 @@
 """
 # Cousins in Binary Tree
 
-In a binary tree, the root node is at depth `0`, and children of each depth `k` node are at depth `k+1`.
+Given the `root` of a binary tree with unique values and the values of two different nodes of the tree `x` and `y`, return `true` *if the nodes corresponding to the values `x` and `y` in the tree are **cousins**, or `false` otherwise*.
 
-Two nodes of a binary tree are cousins if they have the same depth, but have **different parents.** 
+Two nodes of a binary tree are **cousins** if they have the same depth with different parents.
 
-We are given the `root` of a binary tree with unique values, and the values `x` and `y` of two different nodes in the tree.
-
-Return `true` if and only if the nodes corresponding to the values `x` and `y` are cousins.
-
+Note that in a binary tree, the root node is at the depth `0`, and children of each depth `k` node are at the depth `k + 1`.
 
 **Example 1:** 
-![07_q1248-01](./img/993_q1248-01.png)
+![993_q1248-01](./img/993_q1248-01.png)
 ```
 Input: root = [1,2,3,4], x = 4, y = 3
 Output: false
 ```
 
 **Example 2:** 
-![07_q1248-02](./img/993_q1248-02.png)
+![993_q1248-02](./img/993_q1248-02.png)
 ```
 Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
 Output: true
 ```
 
 **Example 3:** 
-![07_q1248-03](./img/993_q1248-03.png)
+![993_q1248-03](./img/993_q1248-03.png)
 ```
 Input: root = [1,2,3,null,4], x = 2, y = 3
 Output: false
 ```
 
-**Note:** 
-    1. The number of nodes in the tree will be between `2` and `100`.
-    2. Each node has a unique integer value from `1` to `100`.
+**Constraints:** 
+    - The number of nodes in the tree is in the range `[2, 100]`.
+    - `1 <= Node.val <= 100` 
+    - Each node has a **unique** value.
+    - `x != y` 
+    - `x` and `y` are exist in the tree.
 """
 
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+import sys
+sys.path = ['.', '../', '../../'] + sys.path
+
+from util import TreeNode, deserializeTree
 
 class Solution:
     def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
@@ -58,7 +63,13 @@ class Solution:
             dfs(tree.left, tree, depth + 1)
             dfs(tree.right, tree, depth + 1)
         dfs(root)
-        print(target1, target2)
         return target1[0] != target2[0] and target1[1] == target2[1]
 
+# False
+print(Solution().isCousins(deserializeTree("[1, 2, 3, 4]"), 4, 3))
 
+# True
+print(Solution().isCousins(deserializeTree("[1, 2, 3, null, 4, null, 5]"), 5, 4))
+
+# False
+print(Solution().isCousins(deserializeTree("[1, 2, 3, null, 4]"), 2, 3))
