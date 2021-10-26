1f97dbfeefbf6a119bd19ab302cb82af493c55da
W-46ec <30034417+W-46ec@users.noreply.github.com>
2021-10-26 14:31:42 -0700

    Question 451 - Optimization

--- Problems/00451-Sort Characters By Frequency.py
+++ Problems/00451-Sort Characters By Frequency.py
@@ -2,54 +2,53 @@
 """
 # Sort Characters By Frequency
 
-Given a string, sort it in decreasing order based on the frequency of characters.
+Given a string `s`, sort it in **decreasing order** based on the **frequency** of the characters. The **frequency** of a character is the number of times it appears in the string.
+
+Return *the sorted string*. If there are multiple answers, return *any of them*.
+
 
 **Example 1:** 
 ```
-Input:
-"tree"
-
-Output:
-"eert"
-
-Explanation:
-'e' appears twice while 'r' and 't' both appear once.
+Input: s = "tree"
+Output: "eert"
+Explanation: 'e' appears twice while 'r' and 't' both appear once.
 So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
 ```
 
 **Example 2:** 
 ```
-Input:
-"cccaaa"
-
-Output:
-"cccaaa"
-
-Explanation:
-Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+Input: s = "cccaaa"
+Output: "aaaccc"
+Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
 Note that "cacaca" is incorrect, as the same characters must be together.
 ```
 
 **Example 3:** 
 ```
-Input:
-"Aabb"
-
-Output:
-"bbAa"
-
-Explanation:
-"bbaA" is also a valid answer, but "Aabb" is incorrect.
+Input: s = "Aabb"
+Output: "bbAa"
+Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
 Note that 'A' and 'a' are treated as two different characters.
 ```
+
+**Constraints:** 
+    - `1 <= s.length <= 5 * 10^5` 
+    - `s` consists of uppercase and lowercase English letters and digits.
 """
+
+from collections import Counter
 
 class Solution:
     def frequencySort(self, s: str) -> str:
-        freq = {c: s.count(c) for c in set(s)}
+        freq = Counter(s)
         keys = sorted(freq, key = lambda x: freq[x], reverse = True)
         return "".join([c * freq[c] for c in keys])
 
-print(Solution().frequencySort("tree"))     # "eetr" or "eert"
-print(Solution().frequencySort("cccaaa"))   # "cccaaa" or "aaaccc"
-print(Solution().frequencySort("Aabb"))     # "bbAa" or "bbaA"
+# "eetr"
+print(Solution().frequencySort("tree"))
+
+# "cccaaa"
+print(Solution().frequencySort("cccaaa"))
+
+# "bbAa"
+print(Solution().frequencySort("Aabb"))
