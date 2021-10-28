95be9831aa7844b58b88220be104f4dad8ae0a2c
W-46ec <30034417+W-46ec@users.noreply.github.com>
2021-10-28 13:52:21 -0700

    Question 15

--- Problems/00015-3Sum.py
+++ Problems/00015-3Sum.py
@@ -2,21 +2,32 @@
 """
 # 3Sum
 
-Given an array `nums` of n integers, are there elements a, b, c in `nums` such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+Given an integer array nums, return all the triplets `[nums[i], nums[j], nums[k]]` such that `i != j`, `i != k`, and `j != k`, and `nums[i] + nums[j] + nums[k] == 0`.
 
-**Note:** 
-The solution set must not contain duplicate triplets.
+Notice that the solution set must not contain duplicate triplets.
 
-**Example:** 
+
+**Example 1:** 
 ```
-Given array nums = [-1, 0, 1, 2, -1, -4],
-
-A solution set is:
-[
-  [-1, 0, 1],
-  [-1, -1, 2]
-]
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
 ```
+
+**Example 2:** 
+```
+Input: nums = []
+Output: []
+```
+
+**Example 3:** 
+```
+Input: nums = [0]
+Output: []
+```
+
+**Constraints:** 
+    - `0 <= nums.length <= 3000` 
+    - `-10^5 <= nums[i] <= 10^5` 
 
 
 **Hint #1** 
