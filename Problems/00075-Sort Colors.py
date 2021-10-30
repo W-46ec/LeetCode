8263292be5c0ff98acbae0e196ab9a480f7d3d58
W-46ec <30034417+W-46ec@users.noreply.github.com>
2021-10-30 14:54:03 -0700

    Question 75 - Add explanation

--- Problems/00075-Sort Colors.py
+++ Problems/00075-Sort Colors.py
@@ -2,22 +2,53 @@
 """
 # Sort Colors
 
-Given an array with *n* objects colored red, white or blue, sort them **in-place** so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+Given an array `nums` with `n` objects colored red, white, or blue, sort them [in-place](https://en.wikipedia.org/wiki/In-place_algorithm) so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
 
-Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+We will use the integers `0`, `1`, and `2` to represent the color red, white, and blue, respectively.
 
-**Note:** You are not suppose to use the library's sort function for this problem.
+You must solve this problem without using the library's sort function.
 
-**Example:** 
+
+**Example 1:** 
 ```
-Input: [2,0,2,1,1,0]
+Input: nums = [2,0,2,1,1,0]
 Output: [0,0,1,1,2,2]
 ```
 
-**Follow up:** 
-    - A rather straight forward solution is a two-pass algorithm using counting sort.
-      First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
-    - Could you come up with a one-pass algorithm using only constant space?
+**Example 2:** 
+```
+Input: nums = [2,0,1]
+Output: [0,1,2]
+```
+
+**Example 3:** 
+```
+Input: nums = [0]
+Output: [0]
+```
+
+**Example 4:** 
+```
+Input: nums = [1]
+Output: [1]
+```
+
+**Constraints:** 
+    - `n == nums.length` 
+    - `1 <= n <= 300` 
+    - `nums[i]` is `0`, `1`, or `2`.
+
+**Follow up**: Could you come up with a one-pass algorithm using only constant extra space?
+
+
+**Hint 1** 
+A rather straight forward solution is a two-pass algorithm using counting sort.
+
+**Hint 2** 
+Iterate the array counting number of 0's, 1's, and 2's.
+
+**Hint 3** 
+Overwrite array with the total number of 0's, then 1's and followed by 2's.
 """
 
 from typing import List
@@ -27,17 +58,32 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        lo, hi, i = 0, len(nums) - 1, 0
-        while i <= hi:
-            if nums[i] == 0:
-                nums[i], nums[lo] = nums[lo], nums[i]
+        # One-pass solution
+        # After sorting, all red objects should be at the front.
+        # All blue objects are at the back 
+        # and the white ones are in the middle.
+        
+        # Maintain two pointers lo, hi:
+        #     - lo <- the position of the last red object + 1
+        #     - hi <- the position of the first blue object - 1
+
+        # Traverse through the list. When we see a red object, place
+        # it at 'lo' and move 'lo' to the right by 1. 
+        # Similarly, when we see a blue object, place it at 'hi' and move
+        # 'hi' to the left by 1.
+
+        lo, hi, idx = 0, len(nums) - 1, 0
+        while idx <= hi:
+            if nums[idx] == 0:      # Red object
+                nums[idx], nums[lo] = nums[lo], nums[idx]
                 lo += 1
-                i = lo if lo > i else i
-            elif nums[i] == 2:
-                nums[i], nums[hi] = nums[hi], nums[i]
+                idx = max(idx, lo)
+            elif nums[idx] == 2:    # Blue object
+                nums[idx], nums[hi] = nums[hi], nums[idx]
                 hi -= 1
-            else:
-                i += 1
+            else:   # White object
+                idx += 1
+
 
 lst = [2, 0, 2, 1, 1, 0]
 print(lst, '-->', end = ' ')
