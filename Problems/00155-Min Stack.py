86b4149ef59697dca0a45698ddd966deb9d651a1
W-46ec <30034417+W-46ec@users.noreply.github.com>
2021-11-01 21:26:11 -0700

    Question 155

---
+++ Problems/00155-Min Stack.py
@@ -0,0 +1,63 @@
+
+"""
+# Min Stack
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the `MinStack` class:
+    - `MinStack()` initializes the stack object.
+    - `void push(int val)` pushes the element val onto the stack.
+    - `void pop()` removes the element on the top of the stack.
+    - `int top()` gets the top element of the stack.
+    - `int getMin()` retrieves the minimum element in the stack.
+
+
+**Example 1:** 
+```
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+```
+
+**Constraints:** 
+    - `-2^31 <= val <= 2^31 - 1` 
+    - `Methods `pop`, `top` and `getMin` operations will always be called on **non-empty** stacks.
+    - `At most `3 * 10^4` calls will be made to `push`, `pop`, `top`, and `getMin`.
+"""
+
+class MinStack:
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.S = []
+
+    def push(self, x: int) -> None:
+        self.S.append(x)
+
+    def pop(self) -> None:
+        if self.S:
+            self.S.pop()
+
+    def top(self) -> int:
+        if self.S:
+            return self.S[-1]
+
+    def getMin(self) -> int:
+        if self.S:
+            return min(self.S)
+
+
