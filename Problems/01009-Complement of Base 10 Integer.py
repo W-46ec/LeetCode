d89ce8028a947d616da552167a32c96d9fd6fdc4
W-46ec <30034417+W-46ec@users.noreply.github.com>
2022-01-03 21:56:03 -0800

    Question 1009 - Optimization

--- Problems/01009-Complement of Base 10 Integer.py
+++ Problems/01009-Complement of Base 10 Integer.py
@@ -2,37 +2,38 @@
 """
 # Complement of Base 10 Integer
 
-Every non-negative integer `N` has a binary representation. For example, `5` can be represented as `"101"` in binary, `11` as `"1011"` in binary, and so on.  Note that except for `N = 0`, there are no leading zeroes in any binary representation.
+The **complement** of an integer is the integer you get when you flip all the `0`'s to `1`'s and all the `1`'s to `0`'s in its binary representation.
 
-The *complement* of a binary representation is the number in binary you get when changing every `1` to a `0` and `0` to a `1`.  For example, the complement of `"101"` in binary is `"010"` in binary.
+    - For example, The integer `5` is `"101"` in binary and its **complement** is `"010"` which is the integer `2`.
 
-For a given number `N` in base-10, return the complement of it's binary representation as a base-10 integer.
+Given an integer `n`, return *its complement*.
 
 
 **Example 1:** 
 ```
-Input: 5
+Input: n = 5
 Output: 2
 Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2 in base-10.
 ```
 
 **Example 2:** 
 ```
-Input: 7
+Input: n = 7
 Output: 0
 Explanation: 7 is "111" in binary, with complement "000" in binary, which is 0 in base-10.
 ```
 
 **Example 3:** 
 ```
-Input: 10
+Input: n = 10
 Output: 5
 Explanation: 10 is "1010" in binary, with complement "0101" in binary, which is 5 in base-10.
 ```
 
-**Note:** 
-    - `0 <= N < 10^9` 
-    - This question is the same as 476: [https://leetcode.com/problems/number-complement/](https://leetcode.com/problems/number-complement/)
+**Constraints:** 
+    - `0 <= n < 10^9` 
+
+Note: This question is the same as 476: [https://leetcode.com/problems/number-complement/](https://leetcode.com/problems/number-complement/) 
 
 **Hint #1** 
 A binary number plus its complement will equal 111....111 in binary. Also, N = 0 is a corner case.
@@ -42,11 +43,11 @@ class Solution:
     def bitwiseComplement(self, N: int) -> int:
         if N == 0:
             return 1
-        mask, x = 0, N
-        while x:
+        mask, neg_n = 0, ~N
+        while N:
             mask = (mask << 1) + 1
-            x >>= 1
-        return (N ^ -1) & mask
+            N >>= 1
+        return mask & neg_n
 
 print(Solution().bitwiseComplement(5))      # 2
 print(Solution().bitwiseComplement(7))      # 0
