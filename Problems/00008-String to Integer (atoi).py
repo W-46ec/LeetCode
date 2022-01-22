7dd66f6fdcbc2404aea74aa87937ea29e6fe6c6b
W-46ec <30034417+W-46ec@users.noreply.github.com>
2022-01-21 19:36:09 -0800

    Question 8 - Optimization

--- Problems/00008-String to Integer (atoi).py
+++ Problems/00008-String to Integer (atoi).py
@@ -2,73 +2,91 @@
 """
 # String to Integer (atoi)
 
-Implement `atoi` which converts a string to an integer.
+Implement the `myAtoi(string s)` function, which converts a string to a 32-bit signed integer (similar to C/C++'s `atoi` function).
 
-The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
-
-The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
-
-If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
-
-If no valid conversion could be performed, a zero value is returned.
+The algorithm for `myAtoi(string s)` is as follows:
+    1. Read in and ignore any leading whitespace.
+    2. Check if the next character (if not already at the end of the string) is `'-'` or `'+'`. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+    3. Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+    4. Convert these digits into an integer (i.e. `"123" -> 123`, `"0032" -> 32`). If no digits were read, then the integer is `0`. Change the sign as necessary (from step 2).
+    5. If the integer is out of the 32-bit signed integer range `[-2^31, 2^31 - 1]`, then clamp the integer so that it remains in the range. Specifically, integers less than `-2^31` should be clamped to `-2^31`, and integers greater than `2^31 - 1` should be clamped to `2^31 - 1`.
+    6. Return the integer as the final result.
 
 **Note:** 
-    - Only the space character `' '` is considered as whitespace character.
-    - Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. If the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
+    - Only the space character `' '` is considered a whitespace character.
+    - **Do not ignore** any characters other than the leading whitespace or the rest of the string after the digits.
 
 **Example 1:** 
 ```
-Input: "42"
+Input: s = "42"
 Output: 42
+Explanation: The underlined characters are what is read in, the caret is the current reader position.
+Step 1: "42" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "42" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "42" ("42" is read in)
+           ^
+The parsed integer is 42.
+Since 42 is in the range [-2^31, 2^31 - 1], the final result is 42.
 ```
 
 **Example 2:** 
 ```
-Input: "   -42"
+Input: s = "   -42"
 Output: -42
-Explanation: The first non-whitespace character is '-', which is the minus sign.
-             Then take as many numerical digits as possible, which gets 42.
+Explanation:
+Step 1: "   -42" (leading whitespace is read and ignored)
+            ^
+Step 2: "   -42" ('-' is read, so the result should be negative)
+             ^
+Step 3: "   -42" ("42" is read in)
+               ^
+The parsed integer is -42.
+Since -42 is in the range [-2^31, 2^31 - 1], the final result is -42.
 ```
 
 **Example 3:** 
 ```
-Input: "4193 with words"
+Input: s = "4193 with words"
 Output: 4193
-Explanation: Conversion stops at digit '3' as the next character is not a numerical digit.
+Explanation:
+Step 1: "4193 with words" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "4193 with words" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "4193 with words" ("4193" is read in; reading stops because the next character is a non-digit)
+             ^
+The parsed integer is 4193.
+Since 4193 is in the range [-2^31, 2^31 - 1], the final result is 4193.
 ```
 
-**Example 4:** 
-```
-Input: "words and 987"
-Output: 0
-Explanation: The first non-whitespace character is 'w', which is not a numerical 
-             digit or a +/- sign. Therefore no valid conversion could be performed.
-```
-
-**Example 5:** 
-```
-Input: "-91283472332"
-Output: -2147483648
-Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
-             Thefore INT_MIN (−231) is returned.
-```
+**Constraints:** 
+    - `0 <= s.length <= 200` 
+    - `s` consists of English letters (lower-case and upper-case), digits `(0-9)`, `' '`, `'+'`, `'-'`, and `'.'`.
 """
 
 class Solution:
     def myAtoi(self, str: str) -> int:
-        ans, lst = 0, list(filter(lambda x: len(x) > 0, str.strip().split(' ')))
-        if len(lst) > 0:
-            idx = 1 if lst[0][0] == '-' or lst[0][0] == '+' else 0
-            while idx < len(lst[0]) and lst[0][idx].isnumeric():
+        ans, idx, sign = 0, 0, 1
+
+        # Strip all white spaces and split the string by space
+        for word in str.strip().split(' '):
+            # Update the sign if the first char is '-'
+            if len(word) and (word[idx] == '-' or word[idx] == '+'):
+                sign = -1 if word[0] == '-' else 1
                 idx += 1
-            w = lst[0][ : idx]
-            ans = int(w) if idx > 0 and w != '-' and w != '+' else 0
-        if ans >= 2 ** 31:
-            return 2 ** 31 - 1
-        elif ans < -(2 ** 31):
-            return -(2 ** 31)
-        else:
-            return ans
+
+            # Keep reading until we reach a non-digit character
+            while idx < len(word) and word[idx].isdigit():
+                ans = ans * 10 + int(word[idx])
+                idx += 1
+
+            # We only need to consider the first word
+            break
+
+        # Clamp the integer so that it remains in [-2^31, 2^31 - 1].
+        return min(max(-2 ** 31, ans * sign), 2 ** 31 - 1)
 
 print(Solution().myAtoi("42"))                  # 42
 print(Solution().myAtoi("   -42"))              # -42
@@ -80,4 +98,6 @@ print(Solution().myAtoi("3.14159"))             # 3
 print(Solution().myAtoi("+1"))                  # 1
 print(Solution().myAtoi("+"))                   # 0
 print(Solution().myAtoi("2147483648"))          # 2147483647
-
+print(Solution().myAtoi("+-12"))                # 0
+print(Solution().myAtoi(""))                    # 0
+print(Solution().myAtoi("00000-42a1234"))       # 0
