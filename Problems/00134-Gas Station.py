ebb98ac7f028ad1bf0422b5c2a1f5cd3b8fb99e0
W-46ec <30034417+W-46ec@users.noreply.github.com>
2022-01-23 21:09:05 -0800

    Question 134 - Optimization

--- Problems/00134-Gas Station.py
+++ Problems/00134-Gas Station.py
@@ -2,25 +2,17 @@
 """
 # Gas Station
 
-There are *N* gas stations along a circular route, where the amount of gas at station *i* is `gas[i]`.
+There are `n` gas stations along a circular route, where the amount of gas at the `ith` station is `gas[i]`.
 
-You have a car with an unlimited gas tank and it costs `cost[i]` of gas to travel from station *i* to its next station (*i*+1). You begin the journey with an empty tank at one of the gas stations.
+You have a car with an unlimited gas tank and it costs `cost[i]` of gas to travel from the `ith` station to its next `(i + 1)th` station. You begin the journey with an empty tank at one of the gas stations.
 
-Return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1.
+Given two integer arrays `gas` and `cost`, return *the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return `-1`*. If there exists a solution, it is **guaranteed** to be **unique**
 
-**Note:** 
-    - If there exists a solution, it is guaranteed to be unique.
-    - Both input arrays are non-empty and have the same length.
-    - Each element in the input arrays is a non-negative integer.
 
 **Example 1:** 
 ```
-Input: 
-gas  = [1,2,3,4,5]
-cost = [3,4,5,1,2]
-
+Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
 Output: 3
-
 Explanation:
 Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
 Travel to station 4. Your tank = 4 - 1 + 5 = 8
@@ -33,12 +25,8 @@ Therefore, return 3 as the starting index.
 
 **Example 2:** 
 ```
-Input: 
-gas  = [2,3,4]
-cost = [3,4,3]
-
+Input: gas = [2,3,4], cost = [3,4,3]
 Output: -1
-
 Explanation:
 You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
 Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
@@ -47,25 +35,41 @@ Travel to station 1. Your tank = 3 - 3 + 3 = 3
 You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
 Therefore, you can't travel around the circuit once no matter where you start.
 ```
+
+**Constraints:** 
+    - `gas.length == n` 
+    - `cost.length == n` 
+    - `1 <= n <= 10^5` 
+    - `0 <= gas[i], cost[i] <= 10^4` 
 """
 
 from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        # O(n^2)
-        length = len(gas)
-        for start in range(length):
-            tank, succ = 0, True
-            for offset in range(length):
-                tank += gas[(start + offset) % length]
-                tank -= cost[(start + offset) % length]
-                if tank < 0:
-                    succ = False
-                    break
-            if succ:
-                return start
-        return -1
+        # # O(n^2)
+        # length = len(gas)
+        # for start in range(length):
+        #     tank, succ = 0, True
+        #     for offset in range(length):
+        #         tank += gas[(start + offset) % length]
+        #         tank -= cost[(start + offset) % length]
+        #         if tank < 0:
+        #             succ = False
+        #             break
+        #     if succ:
+        #         return start
+        # return -1
+
+        # O(n)
+        length, (total, curr, start) = len(gas), [0] * 3
+        for i in range(length):
+            total += gas[i] - cost[i]
+            curr += gas[i] - cost[i]
+            if curr < 0:
+                start = i + 1
+                curr = 0
+        return start if total >= 0 else -1
 
 # 3
 print(Solution().canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]))
