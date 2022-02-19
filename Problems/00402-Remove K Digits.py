f05a02abe6f0fc876ea417a0637ec22ef8f23f08
W-46ec <30034417+W-46ec@users.noreply.github.com>
2022-02-18 16:27:25 -0800

    Question 402 - explanation

--- Problems/00402-Remove K Digits.py
+++ Problems/00402-Remove K Digits.py
@@ -2,11 +2,7 @@
 """
 # Remove K Digits
 
-Given a non-negative integer *num* represented as a string, remove *k* digits from the number so that the new number is the smallest possible.
-
-**Note:** 
-    - The length of *num* is less than 10002 and will be ≥ *k*.
-    - The given *num* does not contain any leading zero.
+Given string num representing a non-negative integer `num`, and an integer `k`, return *the smallest possible integer after removing `k` digits from `num`*.
 
 
 **Example 1:** 
@@ -29,24 +25,50 @@ Input: num = "10", k = 2
 Output: "0"
 Explanation: Remove all the digits from the number and it is left with nothing which is 0.
 ```
+
+**Constraints:** 
+    - `1 <= k <= num.length <= 10^5` 
+    - `num` consists of only digits.
+    - `num` does not have any leading zeros except for the zero itself.
 """
 
 class Solution:
-    def removeKdigits(self, num: str, k: int) -> str:
-        if k >= len(num):
-            return "0"
-        out = []
-        for i in range(len(num)):
-            while k and out and out[-1] > num[i]:
-                out.pop()
+    def removeKdigits(self, num: str, k: int) -> str:        
+        # Let the lenght of num be N, the length of result will be N - k.
+        # To make the result as small as possible, we want the significant 
+        # bits of the result as smaller as possible.
+        # Ideally, the digits in result are in increasing order.
+        
+        # Use a stack to maintain the order
+        stack = []
+        # Start from the most significant bit
+        for x in num:
+            # If the current digit < top of stack, it means we can pop
+            # that larger digit out and replace it with the smaller
+            # digit to obtain a overall smaller result.
+            while k and stack and stack[-1] > x:
+                stack.pop()
                 k -= 1
-            out.append(num[i])
-        while k:
-            out.pop()
-            k -= 1
-        return str(int(''.join(out)))
+            stack.append(x)
+        
+        # If k is still larger than 0, we simply just truncate the in 
+        # the right as the small digits are as far left as possible.
+        # Don't forget to remove the leading zeros.
+        res = "".join(stack[ : len(stack) - k]).lstrip('0')
+        return res if res else '0'
 
+# "1219"
 print(Solution().removeKdigits("1432219", 3))
+
+# "200"
 print(Solution().removeKdigits("10200", 1))
+
+# "0"
 print(Solution().removeKdigits("10", 2))
+
+# "0"
+print(Solution().removeKdigits("10", 1))
+
+# "11"
+print(Solution().removeKdigits("1173", 2))
 
