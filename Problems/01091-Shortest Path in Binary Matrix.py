f8f3e8c05c2a309b86675fc826b3ec0434ec9a8d
W-46ec <30034417+W-46ec@users.noreply.github.com>
2023-05-31 17:45:36 -0700

    Question 1091 - Optimization

--- Problems/01091-Shortest Path in Binary Matrix.py
+++ Problems/01091-Shortest Path in Binary Matrix.py
@@ -2,65 +2,59 @@
 """
 # Shortest Path in Binary Matrix
 
-In an N by N square grid, each cell is either empty (0) or blocked (1).
+Given an `n x n` binary matrix `grid`, return *the length of the shortest clear path in the matrix*. If there is no clear path, return `-1`.
 
-A *clear path from top-left to bottom-right* has length `k` if and only if it is composed of cells `C_1, C_2, ..., C_k` such that:
-    - Adjacent cells `C_i` and `C_{i+1}` are connected 8-directionally (ie., they are different and share an edge or corner)
-    - `C_1` is at location `(0, 0)` (ie. has value `grid[0][0]`)
-    - `C_k` is at location `(N-1, N-1)` (ie. has value `grid[N-1][N-1]`)
-    - If `C_i` is located at `(r, c)`, then `grid[r][c]` is empty (ie. `grid[r][c] == 0`).
+A **clear path** in a binary matrix is a path from the top-left cell (i.e., `(0, 0)`) to the **bottom-right** cell (i.e., `(n - 1, n - 1)`) such that:
+    -All the visited cells of the path are `0`.
+    - All the adjacent cells of the path are **8-directionally** connected (i.e., they are different and they share an edge or a corner).
 
-Return the length of the shortest such clear path from top-left to bottom-right.  If such a path does not exist, return -1.
+The **length of a clear path** is the number of visited cells of this path.
 
 
 **Example 1:** 
-```
-Input: [[0,1],[1,0]]
 ![1091_example1_1](./img/1091_example1_1.png)
-
+```
+Input: grid = [[0,1],[1,0]]
 Output: 2
-![1091_example1_2](./img/1091_example1_2.png)
 ```
 
 **Example 2:** 
-```
-Input: [[0,0,0],[1,1,0],[1,1,0]]
 ![1091_example2_1](./img/1091_example2_1.png)
-
+```
+Input: grid = [[0,0,0],[1,1,0],[1,1,0]]
 Output: 4
-![1091_example2_2](./img/1091_example2_2.png)
 ```
 
-**Note:** 
-    - `1 <= grid.length == grid[0].length <= 100` 
-    - `grid[r][c]` is `0` or `1` 
+**Example 3:** 
+```
+Input: grid = [[1,0,0],[1,1,0],[1,1,0]]
+Output: -1
+```
 
-**Hint #1** 
-Do a breadth first search to find the shortest path.
+**Constraints:** 
+    - `n == grid.length` 
+    - `n == grid[i].length` 
+    - `1 <= n <= 100` 
+    - `grid[i][j] is 0 or 1` 
 """
 
 from typing import List
+from itertools import product
 
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if grid[0][0] == 1:
-            return -1
-        queue = [(0, 0, 1)]     # x, y, cost
-        visited = [[False] * len(grid[0]) for _ in range(len(grid))]
-        visited[0][0] = True
+        n, queue = len(grid), []
+        if grid[0][0] == 0:
+            queue += [(0, 0, 1)]
+            grid[0][0] -= 1
         while queue:
-            x, y, cost = queue[0]
-            queue[0 : 1] = []
-            if x == len(grid) - 1 and y == len(grid[0]) - 1:
-                return cost
-            for dx in range(-1, 2):
-                for dy in range(-1, 2):
-                    if 0 <= x + dx < len(grid) \
-                            and 0 <= y + dy < len(grid[0]) \
-                            and not visited[x + dx][y + dy] \
-                            and grid[x + dx][y + dy] == 0:
-                        visited[x + dx][y + dy] = True
-                        queue[len(queue) : len(queue)] = [(x + dx, y + dy, cost + 1)]
+            x, y, dist = queue.pop(0)
+            if x == n - 1 and y == n - 1:
+                return dist
+            for dx, dy in product(range(-1, 2), range(-1, 2)):
+                if 0 <= x + dx < n and 0 <= y + dy < n and grid[x + dx][y + dy] == 0:
+                    queue += [(x + dx, y + dy, dist + 1)]
+                    grid[x + dx][y + dy] -= 1
         return -1
 
 # 2
