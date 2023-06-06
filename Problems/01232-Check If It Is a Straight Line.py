8572f4f3adc4ba4583d255e59f9698f88496c55e
W-46ec <30034417+W-46ec@users.noreply.github.com>
2023-06-06 00:47:57 -0700

    Question 1232 - Optimization

--- Problems/01232-Check If It Is a Straight Line.py
+++ Problems/01232-Check If It Is a Straight Line.py
@@ -5,21 +5,21 @@
 You are given an array `coordinates`, `coordinates[i] = [x, y]`, where `[x, y]` represents the coordinate of a point. Check if these points make a straight line in the XY plane.
 
 
-Example 1:
-
+**Example 1:** 
+![1232_untitled-diagram-2](./img/1232_untitled-diagram-2.jpg)
 ```
 Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
 Output: true
 ```
 
-Example 2:
-
+**Example 2:** 
+![1232_untitled-diagram-1](./img/1232_untitled-diagram-1.jpg)
 ```
 Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
 Output: false
 ```
 
-Constraints:
+**Constraints:** 
     - `2 <= coordinates.length <= 1000` 
     - `coordinates[i].length == 2` 
     - `-10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4` 
@@ -30,17 +30,18 @@ from typing import List
 
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        if len(coordinates) <= 2:
-            return True
-        x1, y1 = coordinates[0]         # Point 1
-        x2, y2 = coordinates[1]         # Point 2
-        n1, n2 = y1 - y2, x2 - x1       # Norm Vector
-        t = n1 * x1 + n2 * y1           # 2D hyperplane: H_{(x_2 - x_1, y_2 - y_1)}^t
-        for ptr in coordinates[2 : ]:   # Check if other points are on the same line
-            # (x, y) \cdot \vec{n} == t
-            if ptr[0] * n1 + ptr[1] * n2 != t:
-                return False
-        return True
+        # Proof:
+        # ax1 + by1 + c = 0
+        # ax2 + by2 + c = 0
+        # => [a b] * [(x1 - x2) (y1 - y2)]^T = 0
+        # Let b = (x1 - x2), then a = -(y1 - y2) = (y2 - y1)
+        # => c = -(ax1 + by1)
+        # All points in `coordinates` are on the same line iff axi + byi + c is 0.
+
+        [x1, y1], [x2, y2] = coordinates[0], coordinates[1]
+        a, b = y2 - y1, x1 - x2
+        c = -(a * x1 + b * y1)
+        return all([a * xi + b * yi == -c for xi, yi in coordinates])
 
 print(Solution().checkStraightLine([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]))   # True
 print(Solution().checkStraightLine([[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]]))   # False
