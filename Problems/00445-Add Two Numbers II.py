45ecd476dff5eed006be59f872b1a35af4da3843
W-46ec <30034417+W-46ec@users.noreply.github.com>
2023-07-17 15:08:33 -0700

    Question 445 - Optimization

--- Problems/00445-Add Two Numbers II.py
+++ Problems/00445-Add Two Numbers II.py
@@ -23,45 +23,36 @@ Output: 7 -> 8 -> 0 -> 7
 #         self.next = next
 
 import sys
-sys.path  = sys.path = ['.', '../', '../../'] + sys.path
+sys.path  += ['.', '../', '../../']
 
 from util import ListNode, initList, printList
+from typing import Optional
 
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def reverseList(self, head: ListNode) -> ListNode:
         # O(n) time & O(1) space
-        if not l1:
-            return l2
-        elif not l2:
-            return l1
-        num1, num2 = 0, 0
-        while l1 or l2:
-            num1 = num1 * 10 + l1.val if l1 else num1
-            num2 = num2 * 10 + l2.val if l2 else num2
-            l1 = l1.next if l1 else l1
-            l2 = l2.next if l2 else l2
-        head, num = None, num1 + num2
-        if num == 0:
-            return ListNode(0)
-        while num:
-            head, num = ListNode(num % 10, head), num // 10
+        ptr = None
+        while head:
+            ptr, head.next, head = head, ptr, head.next
+        return ptr
+
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        reverse_l1, reverse_l2 = self.reverseList(l1), self.reverseList(l2)
+        head, carry, digit = None, 0, 0
+        while reverse_l1 and reverse_l2:
+            digit = (carry + reverse_l1.val + reverse_l2.val) % 10
+            carry = (carry + reverse_l1.val + reverse_l2.val) // 10
+            head = ListNode(digit, head)
+            reverse_l1, reverse_l2 = reverse_l1.next, reverse_l2.next
+        for lst in [reverse_l1, reverse_l2]:
+            while lst:
+                digit = (carry + lst.val) % 10
+                carry = (carry + lst.val) // 10
+                head = ListNode(digit, head)
+                lst = lst.next
+        if carry != 0:
+            head = ListNode(carry, head)
         return head
-
-
-        # # O(n) time & O(n) space
-        # head, stack1, stack2, carry = None, [], [], 0
-        # while l1 or l2:
-        #     stack1 += [l1.val] if l1 else []
-        #     stack2 += [l2.val] if l2 else []
-        #     l1 = l1.next if l1 else l1
-        #     l2 = l2.next if l2 else l2
-        # while stack1 or stack2:
-        #     num = carry
-        #     num += stack1.pop() if stack1 else 0
-        #     num += stack2.pop() if stack2 else 0
-        #     head, carry = ListNode(num % 10, head), num // 10
-        # head = ListNode(carry, head) if carry else head
-        # return head
 
 # 7 -> 8 -> 0 -> 7 -> NULL
 printList(Solution().addTwoNumbers(initList([7, 2, 4, 3]), initList([5, 6, 4])))
@@ -74,4 +65,7 @@ printList(Solution().addTwoNumbers(initList([]), initList([5])))
 
 # NULL
 printList(Solution().addTwoNumbers(initList([]), initList([])))
+
+# 1 -> 0 -> 0 -> 0 -> NULL
+printList(Solution().addTwoNumbers(initList([9, 9, 9]), initList([1])))
 
