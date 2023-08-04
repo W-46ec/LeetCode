388696d0bb999842e6c7a2007cc817da1d5e6919
W-46ec <30034417+W-46ec@users.noreply.github.com>
2023-08-04 14:08:11 -0700

    Question 139 - Optimization & Explanation

--- Problems/00139-Word Break.py
+++ Problems/00139-Word Break.py
@@ -2,65 +2,71 @@
 """
 # Word Break
 
-Given a **non-empty** string *s* and a dictionary *wordDict* containing a list of **non-empty** words, determine if *s* can be segmented into a space-separated sequence of one or more dictionary words.
+Given a string `s` and a dictionary of strings `wordDict`, return `true` if `s` can be segmented into a space-separated sequence of one or more dictionary words.
 
-**Note:** 
-    - The same word in the dictionary may be reused multiple times in the segmentation.
-    - You may assume the dictionary does not contain duplicate words.
+**Note** that the same word in the dictionary may be reused multiple times in the segmentation.
+
 
 **Example 1:** 
 ```
-Input: s = "leetcode", wordDict = ["leet", "code"]
+Input: s = "leetcode", wordDict = ["leet","code"]
 Output: true
 Explanation: Return true because "leetcode" can be segmented as "leet code".
 ```
 
 **Example 2:** 
 ```
-Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Input: s = "applepenapple", wordDict = ["apple","pen"]
 Output: true
 Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
-             Note that you are allowed to reuse a dictionary word.
+Note that you are allowed to reuse a dictionary word.
 ```
 
 **Example 3:** 
 ```
-Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
 Output: false
 ```
+
+**Constraints:** 
+    - `1 <= s.length <= 300` 
+    - `1 <= wordDict.length <= 1000` 
+    - `1 <= wordDict[i].length <= 20` 
+    - `s` and `wordDict[i]` consist of only lowercase English letters.
+    - All the strings of `wordDict` are unique.
 """
 
 from typing import List
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # # Time Limit Exceeded
-        # if s == "":
-        #     return True
-        # for i in range(len(s)):
-        #     word = s[ : i + 1]
-        #     # print(word)
-        #     if word != "" and word in wordDict and self.wordBreak(s[i + 1 : ], wordDict):
-        #         return True
-        # return False
-
-        
-        # Reference: https://leetcode.com/problems/word-break/discuss/870794/Faster-than-99.6-Python
-        if any([c not in set(''.join(wordDict)) for c in s]):
+        # If any character in s is not in any of the word in wordDict, return False
+        if not set(s).issubset(set("".join(wordDict))):
             return False
-        
-        wordDict = sorted(wordDict, key = len, reverse = True)
-        seen = set()
+
+        # Convert wordDict into a set to allow faster look up.
+        # 'invalid_strings' stores the sub-strings of s that
+        # cannot be segmented into a sequence of wordDict words.
+        wordDict, invalid_strings = set(wordDict), set()
 
         def backtracking(s, wordDict):
+            # Base case: If s is one of the words in wordDict, return True
             if s in wordDict:
                 return True
-            if s in seen:
+
+            # Early stopping: If we have seen s before and s cannot be
+            # segmented into a sequence of wordDict words, return False
+            if s in invalid_strings:
                 return False
-            seen.add(s)
+
+            # Backtracking
             for w in wordDict:
                 if s.startswith(w) and backtracking(s[len(w) : ], wordDict):
                     return True
+
+            # s cannot be segmented into a sequence of wordDict words.
+            # Add it to 'invalid_strings', and return False
+            invalid_strings.add(s)
             return False
 
         return backtracking(s, wordDict)
