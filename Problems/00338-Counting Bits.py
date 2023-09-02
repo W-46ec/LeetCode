c2a6738d883ecce0d2d1851878a3418aa97176a3
W-46ec <30034417+W-46ec@users.noreply.github.com>
2023-09-02 03:39:14 -0700

    Question 338 - Optimization

--- Problems/00338-Counting Bits.py
+++ Problems/00338-Counting Bits.py
@@ -2,61 +2,67 @@
 """
 # Counting Bits
 
-Given a non negative integer number **num**. For every numbers **i** in the range **0 ≤ i ≤ num** calculate the number of 1's in their binary representation and return them as an array.
+Given an integer `n`, return *an array `ans` of length `n + 1` such that for each `i` (`0 <= i <= n`), `ans[i]` is the **number of** `1`'s in the binary representation of `i`.
+
 
 **Example 1:** 
 ```
-Input: 2
+Input: n = 2
 Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
 ```
 
 **Example 2:** 
 ```
-Input: 5
+Input: n = 5
 Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
 ```
 
+**Constraints:** 
+    - `0 <= n <= 10^5` 
+
+
 **Follow up:** 
-    - It is very easy to come up with a solution with run time **O(n*sizeof(integer))**. But can you do it in linear time **O(n)** /possibly in a single pass?
-    - Space complexity should be **O(n)**.
-    - Can you do it like a boss? Do it without using any builtin function like **__builtin_popcount** in c++ or in any other language.
+    - It is very easy to come up with a solution with a runtime of `O(n log n)`. Can you do it in linear time `O(n)` and possibly in a single pass?
+    - Can you do it without using any built-in function (i.e., like `__builtin_popcount` in C++)?
 """
 
 from typing import List
-import numpy as np
 
 class Solution:
-    def countBits(self, num: int) -> List[int]:
-        # # O(n * sizeof(integer))
-        # ans = []
-        # for i in range(num + 1):
-        #     curr = 0
-        #     while i > 0:
-        #         curr += 0x1 & i
-        #         i >>= 1
-        #     ans.append(curr)
-        # return ans
-
-        # O(n)
-        # ans, exp = [0, 1], 1
-        # while 2 ** exp - 1 < num:
-        #     arr = ans[2 ** (exp - 1) : 2 ** exp]
-        #     ans += arr + [x + 1 for x in arr]
-        #     exp += 1
-        # return ans[ : num + 1]
-
-        # O(n) - Using numpy
-        ans, exp = np.arange(2, dtype = 'int'), 1
-        while 2 ** exp - 1 < num:
-            ans = np.concatenate((ans, ans + 1))
+    def countBits(self, n: int) -> List[int]:
+        ans, exp = [0], 0
+        while 2 ** exp - 1 < n:
+            ans += [x + 1 for x in ans]
             exp += 1
-        return ans[ : num + 1]
+        return ans[ : n + 1]
 
+# [0]
 print(Solution().countBits(0))
+
+# [0, 1]
 print(Solution().countBits(1))
+
+# [0, 1, 1]
 print(Solution().countBits(2))
+
+# [0, 1, 1, 2]
 print(Solution().countBits(3))
+
+# [0, 1, 1, 2, 1]
 print(Solution().countBits(4))
+
+# [0, 1, 1, 2, 1, 2]
 print(Solution().countBits(5))
 
 # for i, n in enumerate(Solution().countBits(100)):
