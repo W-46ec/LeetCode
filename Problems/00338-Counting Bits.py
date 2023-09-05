bccdaf6b099a7f68e924064711fd2daa86b8a4e5
W-46ec <30034417+W-46ec@users.noreply.github.com>
2023-09-04 21:48:41 -0700

    Question 338 - Explanation

--- Problems/00338-Counting Bits.py
+++ Problems/00338-Counting Bits.py
@@ -41,6 +41,30 @@ from typing import List
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        """
+        Consider the following binary representation: (b_k, b_k-1, ..., b_1, b_0), where b_i is either 0 or 1.
+
+        We have the following properties:
+            When decimal number i is in the range 1 <= i <= 1, b_0 is equal to 1;
+            When decimal number i is in the range 2 <= i <= 3, b_1 is equal to 1;
+            When decimal number i is in the range 4 <= i <= 7, b_2 is equal to 1;
+            ...
+            When decimal number i is in the range 2 ^ k <= i <= 2 ^ (k + 1) - 1, b_k is equal to 1;
+
+        So, if we have the solution for numbers in range 0 <= i <= 2 ^ (k + 1) - 1 (namaly S_k), 
+        we can easily obtain the solution for numbers in range 2 ^ (k + 1) <= i <= 2 ^ (k + 2) - 1
+        by adding 1 to every element in S_k, because each number in range 2 ^ (k + 1) <= i <= 2 ^ (k + 2) - 1
+        has exactly one extra binary '1' compared to numbers in range 0 <= i <= 2 ^ (k + 1) - 1
+        (i.e., setting b_k to 1).
+
+        For example, the base case if when i = 0, solution S_0 is [0].
+        When 1 <= i <= 1, we add 1 to every element in S_0 = [0], which gives us [1] (since we are setting b_0 to 1).
+        So S_1 (i.e., 0 <= i <= 1) is the concatenation of S_0 and [1], which is [0, 1].
+        When 2 <= i <= 3, we add 1 to every element in S_1 = [0, 1], which gives us [1, 2] (we are setting b_1 to 1).
+        So S_2 (i.e., 0 <= i <= 3) is the concatenation of S_1 and [1, 2], which is [0, 1, 1, 2].
+        ...
+        We may keep doing this until we cover n.
+        """
         ans, exp = [0], 0
         while 2 ** exp - 1 < n:
             ans += [x + 1 for x in ans]
