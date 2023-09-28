a947435eb10ebadcbf7bf77e5d024725c4d41834
W-46ec <30034417+W-46ec@users.noreply.github.com>
2023-09-28 14:59:11 -0700

    Question 905 - Optimization

--- Problems/00905-Sort Array By Parity.py
+++ Problems/00905-Sort Array By Parity.py
@@ -2,28 +2,49 @@
 """
 # Sort Array By Parity
 
-Given an array `A` of non-negative integers, return an array consisting of all the even elements of `A`, followed by all the odd elements of `A`.
+Given an integer array `nums`, move all the even integers at the beginning of the array followed by all the odd integers.
 
-You may return any answer array that satisfies this condition.
+Return ***any array** that satisfies this condition*.
 
 
 **Example 1:** 
 ```
-Input: [3,1,2,4]
+Input: nums = [3,1,2,4]
 Output: [2,4,3,1]
-The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
 ```
 
-**Note:** 
-    1. `1 <= A.length <= 5000` 
-    2. `0 <= A[i] <= 5000` 
+**Example 2:** 
+```
+Input: nums = [0]
+Output: [0]
+```
+
+**Constraints:** 
+    - `1 <= nums.length <= 5000` 
+    - `0 <= nums[i] <= 5000` 
 """
 
 from typing import List
 
 class Solution:
-    def sortArrayByParity(self, A: List[int]) -> List[int]:
-        return sorted(A, key = lambda x: x % 2)
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        # # Built-in function
+        # return sorted(nums, key = lambda x: x % 2)
 
-print(Solution().sortArrayByParity([3, 1, 2, 4]))   # [2, 4, 3, 1]
+        # Swapping
+        lo, hi = 0, len(nums) - 1
+        while lo < hi:
+            while lo < hi and nums[lo] % 2 == 0:
+                lo += 1
+            while lo < hi and nums[hi] % 2 == 1:
+                hi -= 1
+            nums[lo], nums[hi] = nums[hi], nums[lo]
+        return nums
+
+# [4, 2, 1, 3]
+print(Solution().sortArrayByParity([3, 1, 2, 4]))
+
+# [0]
+print(Solution().sortArrayByParity([0]))
 
