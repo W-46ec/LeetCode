7f9ea9dbc9a952b4678946dd05b0a688a1278838
W-46ec <30034417+W-46ec@users.noreply.github.com>
2023-10-15 22:01:52 -0700

    Question 119 - Optimization - Binomial theorem

--- Problems/00119-Pascal's Triangle II.py
+++ Problems/00119-Pascal's Triangle II.py
@@ -2,35 +2,68 @@
 """
 # Pascal's Triangle II
 
-Given a non-negative index *k* where *k* ≤ 33, return the *k*^th index row of the Pascal's triangle.
+Given an integer `rowIndex`, return the `rowIndexth` (**0-indexed**) row of the **Pascal's triangle**.
 
-Note that the row index starts from 0.
+In **Pascal's triangle**, each number is the sum of the two numbers directly above it as shown:
 
 ![119_PascalTriangleAnimated2](./img/119_PascalTriangleAnimated2.gif)
 
 In Pascal's triangle, each number is the sum of the two numbers directly above it.
 
-**Example:** 
+**Example 1:** 
 ```
-Input: 3
+Input: rowIndex = 3
 Output: [1,3,3,1]
 ```
 
-**Follow up:** 
-Could you optimize your algorithm to use only *O(k)* extra space?
+**Example 2:** 
+```
+Input: rowIndex = 0
+Output: [1]
+```
+
+**Example 3:** 
+```
+Input: rowIndex = 1
+Output: [1,1]
+```
+
+**Constraints:** 
+    - `0 <= rowIndex <= 33` 
+
+
+**Follow up**: Could you optimize your algorithm to use only `O(rowIndex)` extra space?
 """
 
 from typing import List
+from math import comb
 
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        ans = []
-        while rowIndex + 1:
-            for j in range(len(ans) - 1):
-                ans[j] = ans[j] + ans[j + 1]
-            ans = [1] + ans
-            rowIndex -= 1
-        return ans
+        # # Iterative solution
+        # row = []
+        # while rowIndex + 1:
+        #     for j in range(len(row) - 1):
+        #         row[j] = row[j] + row[j + 1]
+        #     row = [1] + row
+        #     rowIndex -= 1
+        # return row
 
-print(Solution().getRow(3))     # [1, 3, 3, 1]
+        # Math - Binomial theorem
+        # (a + b)^n = \Sigma_{i = 0}^{n} {{n}\choose{i}} a^{i} b^{n - i}
+        return [comb(rowIndex, i) for i in range(rowIndex + 1)]
 
+# [1]
+print(Solution().getRow(0))
+
+# [1, 1]
+print(Solution().getRow(1))
+
+# [1, 2, 1]
+print(Solution().getRow(2))
+
+# [1, 3, 3, 1]
+print(Solution().getRow(3))
+
+# [1, 4, 6, 4, 1]
+print(Solution().getRow(4))
