7fec06565273e15c2f0fd15203df45146a26f83f
W-46ec <30034417+W-46ec@users.noreply.github.com>
2023-10-18 23:59:41 -0700

    Question 844

---
+++ Problems/00844-Backspace String Compare.py
@@ -0,0 +1,58 @@
+
+"""
+# Backspace String Compare
+
+Given two strings `s` and `t`, return *`true` if they are equal when both are typed into empty text editors*. `'#'` means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+
+**Example 1:** 
+```
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+```
+
+**Example 2:** 
+```
+Input: s = "ab##", t = "c#d#"
+Output: true
+Explanation: Both s and t become "".
+```
+
+**Example 3:** 
+```
+Input: s = "a#c", t = "b"
+Output: false
+Explanation: s becomes "c" while t becomes "b".
+```
+
+**Constraints:** 
+    - `1 <= s.length, t.length <= 200` 
+    - `s` and `t` only contain lowercase letters and `'#'` characters.
+
+
+**Follow up**: Can you solve it in `O(n)` time and `O(1)` space?
+"""
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stack_s, stack_t = [], []
+        for string, stack in [[s, stack_s], [t, stack_t]]:
+            for c in string:
+                if c == '#':
+                    if stack:
+                        stack.pop()
+                else:
+                    stack.append(c)
+        return "".join(stack_s) == "".join(stack_t)
+
+# True
+print(Solution().backspaceCompare("ab#c", "ad#c"))
+
+# True
+print(Solution().backspaceCompare("ab##", "c#d#"))
+
+# False
+print(Solution().backspaceCompare("a#c", "b"))
