cbe3cb8f3cc58c92fc479dea3d7a8e035a37ae7e
W-46ec <30034417+W-46ec@users.noreply.github.com>
2024-01-15 14:40:45 -0800

    Remove '30-Day LeetCoding Challenge' and 'Contest'

--- Contest/Contest123/989-addToArrayForm.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-	def addToArrayForm(self, A: 'List[int]', K: 'int') -> 'List[int]':
-		return [int(j) for j in list(str(int("".join([str(i) for i in A])) + K))]
-		
-
-print(Solution().addToArrayForm([9, 9, 9], 1))
--- Contest/Contest168/5291-findNumbers.py
+++
@@ -1,9 +0,0 @@
-
-from typing import List
-
-class Solution:
-	def findNumbers(self, nums: List[int]) -> int:
-		return len([i for i in nums if len(str(i)) % 2 == 0])
-
-print(Solution().findNumbers([12, 345, 2, 6, 7896]))
-print(Solution().findNumbers([555, 901, 482, 1771]))
