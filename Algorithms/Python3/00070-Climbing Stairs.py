7ee977ff84d234c8e91a629cc0e103d46f94302e
W-46ec <30034417+W-46ec@users.noreply.github.com>
2024-01-18 00:38:42 -0800

    Question 70 - Python - Test

--- Algorithms/Python3/00070-Climbing Stairs.py
+++ Algorithms/Python3/00070-Climbing Stairs.py
@@ -32,6 +32,8 @@ Explanation: There are three ways to climb to the top.
 To reach nth step, what could have been your previous steps? (Think about the step sizes)
 """
 
+import unittest
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         # Let F(n) be the solution for climbing n steps
@@ -45,11 +47,16 @@ class Solution:
             a, b = b, a + b
         return a
 
-# # ANS:
-# 1
-# 2
-# 3
-# 5
-# 8
-for i in range(1, 6):
-    print(Solution().climbStairs(i))
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.soln_obj = Solution()
+        self.answers = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903]
+
+    def testcase1(self):
+        for i in range(1, 46):
+            self.assertEqual(self.soln_obj.climbStairs(i), self.answers[i - 1])
+
+
+if __name__ == '__main__':
+    unittest.main()
