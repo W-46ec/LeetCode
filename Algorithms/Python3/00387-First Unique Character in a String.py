45efb6c0a8d854c34258af57adc10a20484c292e
W-46ec <30034417+W-46ec@users.noreply.github.com>
2024-02-04 22:06:43 -0800

    Question 387 - Python - Optimization & Tests

--- Algorithms/Python3/00387-First Unique Character in a String.py
+++ Algorithms/Python3/00387-First Unique Character in a String.py
@@ -2,36 +2,60 @@
 """
 # First Unique Character in a String
 
-Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+Given a string `s`, *find the first non-repeating character in it and return its index*. If it does not exist, return `-1`.
 
-**Examples:** 
+
+**Example 1:** 
 ```
-s = "leetcode"
-return 0.
-
-s = "loveleetcode",
-return 2.
+Input: s = "leetcode"
+Output: 0
 ```
 
-**Note:** You may assume the string contain only lowercase letters.
+**Example 2:** 
+```
+Input: s = "loveleetcode"
+Output: 2
+```
+
+**Example 3:** 
+```
+Input: s = "aabb"
+Output: -1
+```
+
+**Constraints:** 
+    - `1 <= s.length <= 10^5` 
+    - `s` consists of only lowercase English letters.
 """
 
-
-from collections import OrderedDict
+import unittest
+from collections import Counter
 
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        d, pool = OrderedDict(), set()
-        for i in range(len(s)):
-            if s[i] not in d and s[i] not in pool:
-                d[s[i]] = i
-            elif s[i] in d:
-                d.pop(s[i])
-                pool.add(s[i])
-        return next(iter(d.items()))[1] if len(d) > 0 else -1
+        freq = Counter(s)
+        for i, c in enumerate(s):
+            if freq[c] == 1:
+                return i
+        return -1
 
-print(Solution().firstUniqChar("leetcode"))         # 0
-print(Solution().firstUniqChar("loveleetcode"))     # 2
-print(Solution().firstUniqChar("aaaaa"))            # -1
-print(Solution().firstUniqChar("aadadaad"))         # -1
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.soln_obj = Solution()
+
+    def testcase1(self):
+        self.assertEqual(self.soln_obj.firstUniqChar("leetcode"), 0)
+
+    def testcase2(self):
+        self.assertEqual(self.soln_obj.firstUniqChar("loveleetcode"), 2)
+
+    def testcase3(self):
+        self.assertEqual(self.soln_obj.firstUniqChar("aabb"), -1)
+
+    def testcase4(self):
+        self.assertEqual(self.soln_obj.firstUniqChar("aadadaad"), -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
