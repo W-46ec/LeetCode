38436f57611997b2bd1db87d4afda9a2a0805ba9
W-46ec <30034417+W-46ec@users.noreply.github.com>
2024-02-08 11:50:29 -0800

    Question 176 - MySQL

---
+++ Database/MySQL/00176- Second Highest Salary.sql
@@ -0,0 +1,71 @@
+
+/*
+#  Second Highest Salary
+
+Table: `Employee` 
+```
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+```
+
+Write a solution to find the second highest salary from the `Employee` table. If there is no second highest salary, return `null (return None in Pandas)`.
+
+The result format is in the following example.
+
+
+**Example 1:** 
+```
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+```
+*/
+
+/*
+# SQL Schema
+
+Create table If Not Exists Employee (id int, salary int)
+Truncate table Employee
+insert into Employee (id, salary) values ('1', '100')
+insert into Employee (id, salary) values ('2', '200')
+insert into Employee (id, salary) values ('3', '300')
+*/
+
+
+# Write your MySQL query statement below
+SELECT MAX(E.salary) AS SecondHighestSalary
+FROM Employee AS E
+WHERE E.salary <> (SELECT MAX(salary) FROM Employee)
