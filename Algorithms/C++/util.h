220689ac7ec6b12a9a4fadea0b5d222831ea915e
W-46ec <30034417+W-46ec@users.noreply.github.com>
2024-06-02 16:46:33 -0700

    util.h - C++ - modify print() function

--- Algorithms/C++/util.h
+++ Algorithms/C++/util.h
@@ -26,3 +26,25 @@ void print(const std::vector<std::vector<T>> &matrix, const std::string &endline
     }
     std::cout << "}" << endline;
 }
+
+// Display a 1D vector
+template <class T>
+void print(const std::vector<T> &arr, const std::string &endline = "\n", const char &quote = '\0') {
+    std::cout << "{";
+    for (unsigned i = 0; i < arr.size(); i++) {
+        std::cout << quote << arr[i] << quote;
+        i < arr.size() - 1 ? std::cout << ", " : std::cout << "";
+    }
+    std::cout << "}" << endline;
+}
+
+// Display a 2D vector
+template <class T>
+void print(const std::vector<std::vector<T>> &matrix, const std::string &endline = "\n", const char &quote = '\0') {
+    std::cout << "{";
+    for (unsigned i = 0; i < matrix.size(); i++) {
+        print<T>(matrix[i], "", quote);
+        i < matrix.size() - 1 ? std::cout << ", " : std::cout << "";
+    }
+    std::cout << "}" << endline;
+}
