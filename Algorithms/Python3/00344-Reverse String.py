74993f1d38e6ddf02e98bcc861e228db957de6bf
W-46ec <30034417+W-46ec@users.noreply.github.com>
2024-06-02 15:51:27 -0700

    Question 344 - Python - Tests

--- Algorithms/Python3/00344-Reverse String.py
+++ Algorithms/Python3/00344-Reverse String.py
@@ -2,11 +2,9 @@
 """
 # Reverse String
 
-Write a function that reverses a string. The input string is given as an array of characters `char[]`.
+Write a function that reverses a string. The input string is given as an array of characters `s`.
 
-Do not allocate extra space for another array, you must do this by **modifying the input array in-place** with O(1) extra memory.
-
-You may assume all the characters consist of printable ascii characters.
+You must do this by modifying the input array [in-place](https://en.wikipedia.org/wiki/In-place_algorithm) with `O(1)` extra memory.
 
 
 **Example 1:** 
@@ -20,8 +18,14 @@ Output: ["o","l","l","e","h"]
 Input: ["H","a","n","n","a","h"]
 Output: ["h","a","n","n","a","H"]
 ```
+
+**Constraints:** 
+    - `1 <= s.length <= 10^5` 
+    - `s[i]` is a [printable ascii character](https://en.wikipedia.org/wiki/ASCII#Printable_characters).
 """
 
+import unittest
+from random import randint, choices
 from typing import List
 
 class Solution:
@@ -32,11 +36,34 @@ class Solution:
         for i in range(len(s) // 2):
             s[i], s[len(s) - i - 1] = s[len(s) - i - 1], s[i]
 
-lst = ['h', 'e', 'l', 'l', 'o']
-Solution().reverseString(lst)
-print(lst)
 
-lst = ['H', 'a', 'n', 'n', 'a', 'h']
-Solution().reverseString(lst)
-print(lst)
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.soln_obj = Solution()
+
+    def testcase1(self):
+        lst = ['h', 'e', 'l', 'l', 'o']
+        expected = lst[::-1]
+        self.soln_obj.reverseString(lst)
+        self.assertEqual(lst, expected)
+
+    def testcase2(self):
+        lst = ['H', 'a', 'n', 'n', 'a', 'h']
+        expected = lst[::-1]
+        self.soln_obj.reverseString(lst)
+        self.assertEqual(lst, expected)
+
+    def test_random(self):
+        num_tests = 100
+        printable_chars = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
+        for _ in range(num_tests):
+            length = randint(1, 10 ** 5)
+            lst = choices(printable_chars, k = length)
+            expected = lst[::-1]
+            self.soln_obj.reverseString(lst)
+            self.assertEqual(lst, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
