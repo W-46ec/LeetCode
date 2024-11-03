d1aca0eca352112654d789de363a1e94782d5a0f
W-46ec <30034417+W-46ec@users.noreply.github.com>
2024-11-03 13:58:09 -0800

    Question 2490 - C++

---
+++ Algorithms/C++/02490-Circular Sentence.cpp
@@ -0,0 +1,87 @@
+
+/*
+# Circular Sentence
+
+A **sentence** is a list of words that are separated by a **single** space with no leading or trailing spaces.
+    - For example, `"Hello World"`, `"HELLO"`, `"hello world hello world"` are all sentences.
+
+Words consist of **only** uppercase and lowercase English letters. Uppercase and lowercase English letters are considered different.
+
+A sentence is **circular** if:
+    - The last character of a word is equal to the first character of the next word.
+    - The last character of the last word is equal to the first character of the first word.
+
+For example, `"leetcode exercises sound delightful"`, `"eetcode"`, `"leetcode eats soul"` are all circular sentences. However, `"Leetcode is cool"`, `"happy Leetcode"`, `"Leetcode"` and `"I like Leetcode"` are **not** circular sentences.
+
+Given a string `sentence`, return *`true` if it is circular*. Otherwise, return `false`.
+
+
+**Example 1:** 
+```
+Input: sentence = "leetcode exercises sound delightful"
+Output: true
+Explanation: The words in sentence are ["leetcode", "exercises", "sound", "delightful"].
+- leetcode's last character is equal to exercises's first character.
+- exercises's last character is equal to sound's first character.
+- sound's last character is equal to delightful's first character.
+- delightful's last character is equal to leetcode's first character.
+The sentence is circular.
+```
+
+**Example 2:** 
+```
+Input: sentence = "eetcode"
+Output: true
+Explanation: The words in sentence are ["eetcode"].
+- eetcode's last character is equal to eetcode's first character.
+The sentence is circular.
+```
+
+**Example 3:** 
+```
+Input: sentence = "Leetcode is cool"
+Output: false
+Explanation: The words in sentence are ["Leetcode", "is", "cool"].
+- Leetcode's last character is not equal to is's first character.
+The sentence is not circular.
+```
+
+**Constraints:** 
+    - `1 <= sentence.length <= 500` 
+    - `sentence` consist of only lowercase and uppercase English letters and spaces.
+    - The words in `sentence` are separated by a single space.
+    - There are no leading or trailing spaces.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        unsigned i = 0, j = 0;
+        while (i < sentence.length()) {
+            j = i + 1;
+            while (j < sentence.length() && sentence[j] != ' ') {
+                j++;
+            }
+            if (j + 1 < sentence.length() and sentence[j - 1] != sentence[j + 1]) {
+                return false;
+            }
+            i = j + 1;
+        }
+        return sentence[sentence.length() - 1] == sentence[0];
+    }
+};
+
+
+int main(int argc, char *argv[]) {
+    Solution obj;
+    cout << obj.isCircularSentence(string("leetcode exercises sound delightful")) << endl;  // 1
+    cout << obj.isCircularSentence(string("eetcode")) << endl;                              // 1
+    cout << obj.isCircularSentence(string("Leetcode is cool")) << endl;                     // 0
+    cout << obj.isCircularSentence(string("cool")) << endl;                                 // 0
+    cout << obj.isCircularSentence(string("l")) << endl;                                    // 1
+    return 0;
+}
