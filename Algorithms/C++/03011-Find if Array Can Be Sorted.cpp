52b877963147bd366dad753b4513acb31d6f33b5
W-46ec <30034417+W-46ec@users.noreply.github.com>
2024-11-06 22:39:22 -0800

    Question 3011 - C++

---
+++ Algorithms/C++/03011-Find if Array Can Be Sorted.cpp
@@ -0,0 +1,98 @@
+
+/*
+# Find if Array Can Be Sorted
+
+You are given a **0-indexed** array of **positive** integers `nums`.
+
+In one **operation**, you can swap any two **adjacent** elements if they have the same number of set bits. You are allowed to do this operation **any** number of times (**including zero**).
+
+Return *`true` if you can sort the array, else return `false`*.
+
+
+**Example 1:** 
+```
+Input: nums = [8,4,2,30,15]
+Output: true
+Explanation: Let's look at the binary representation of every element. The numbers 2, 4, and 8 have one set bit each with binary representation "10", "100", and "1000" respectively. The numbers 15 and 30 have four set bits each with binary representation "1111" and "11110".
+We can sort the array using 4 operations:
+- Swap nums[0] with nums[1]. This operation is valid because 8 and 4 have one set bit each. The array becomes [4,8,2,30,15].
+- Swap nums[1] with nums[2]. This operation is valid because 8 and 2 have one set bit each. The array becomes [4,2,8,30,15].
+- Swap nums[0] with nums[1]. This operation is valid because 4 and 2 have one set bit each. The array becomes [2,4,8,30,15].
+- Swap nums[3] with nums[4]. This operation is valid because 30 and 15 have four set bits each. The array becomes [2,4,8,15,30].
+The array has become sorted, hence we return true.
+Note that there may be other sequences of operations which also sort the array.
+```
+
+**Example 2:** 
+```
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: The array is already sorted, hence we return true.
+```
+
+**Example 3:** 
+```
+Input: nums = [3,16,8,4,2]
+Output: false
+Explanation: It can be shown that it is not possible to sort the input array using any number of operations.
+```
+
+**Constraints:** 
+    - `1 <= nums.length <= 100` 
+    - `1 <= nums[i] <= 28` 
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+private:
+    unsigned bit_count(int x) {
+        unsigned count = 0;
+        while (x) {
+            count += x & 0x1;
+            x >>= 1;
+        }
+        return count;
+    }
+
+public:
+    bool canSortArray(vector<int>& nums) {
+        unsigned i = 0, j = 0;
+        int prev_max = -1;
+        while (i < nums.size()) {
+            unsigned curr_bit_count = bit_count(nums[i]);
+            int curr_min = nums[i], curr_max = nums[i];
+            j = i + 1;
+            while (j < nums.size() && bit_count(nums[j]) == curr_bit_count) {
+                curr_min = min(curr_min, nums[j]);
+                curr_max = max(curr_max, nums[j]);
+                j += 1;
+            }
+            if (curr_min < prev_max) {
+                return false;
+            }
+            prev_max = curr_max;
+            i = j;
+        }
+        return true;
+    }
+};
+
+
+int main(int argc, char *argv[]) {
+    Solution obj;
+
+    vector<int> nums = {8, 4, 2, 30, 15};
+    cout << obj.canSortArray(nums) << endl;     // 1
+
+    nums = {1, 2, 3, 4, 5};
+    cout << obj.canSortArray(nums) << endl;     // 1
+
+    nums = {3, 16, 8, 4, 2};
+    cout << obj.canSortArray(nums) << endl;     // 0
+
+    return 0;
+}
