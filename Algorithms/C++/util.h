965f3afae177efbc161249b5c13ff1874e65fc71
W-46ec <30034417+W-46ec@users.noreply.github.com>
2024-11-09 14:46:53 -0800

    utils - C++ - Remove outdated code

--- Algorithms/C++/util.h
+++ Algorithms/C++/util.h
@@ -5,27 +5,6 @@
 #include <string>
 #include <vector>
 
-// Display a 1D vector
-template <class T>
-void print(const std::vector<T> &arr, const std::string &endline = "\n", const bool &quote = false) {
-    std::cout << "{";
-    for (unsigned i = 0; i < arr.size(); i++) {
-        quote ? std::cout << "\"" << arr[i] << "\"" : std::cout << arr[i];
-        i < arr.size() - 1 ? std::cout << ", " : std::cout << "";
-    }
-    std::cout << "}" << endline;
-}
-
-// Display a 2D vector
-template <class T>
-void print(const std::vector<std::vector<T>> &matrix, const std::string &endline = "\n", const bool &quote = false) {
-    std::cout << "{";
-    for (unsigned i = 0; i < matrix.size(); i++) {
-        print<T>(matrix[i], "", quote);
-        i < matrix.size() - 1 ? std::cout << ", " : std::cout << "";
-    }
-    std::cout << "}" << endline;
-}
 
 // Display a 1D vector
 template <class T>
