f1df8376077fcdacbfbcaa28daf0a26cf07a07fc
W-46ec <30034417+W-46ec@users.noreply.github.com>
2024-11-20 23:37:53 -0800

    Question 2257 - Python

---
+++ Algorithms/Python3/02257-Count Unguarded Cells in the Grid.py
@@ -0,0 +1,92 @@
+
+"""
+# Count Unguarded Cells in the Grid
+
+You are given two integers `m` and `n` representing a **0-indexed** `m x n` grid. You are also given two 2D integer arrays `guards` and `walls` where `guards[i] = [row_i, col_i]` and `walls[j] = [row_j, col_j]` represent the positions of the `ith` guard and `jth` wall respectively.
+
+A guard can see **every** cell in the four cardinal directions (north, east, south, or west) starting from their position unless **obstructed** by a wall or another guard. A cell is **guarded** if there is **at least** one guard that can see it.
+
+Return *the number of unoccupied cells that are **not guarded***.
+
+
+**Example 1:** 
+
+```
+Input: m = 4, n = 6, guards = [[0,0],[1,1],[2,3]], walls = [[0,1],[2,2],[1,4]]
+Output: 7
+Explanation: The guarded and unguarded cells are shown in red and green respectively in the above diagram.
+There are a total of 7 unguarded cells, so we return 7.
+```
+
+**Example 2:** 
+
+```
+Input: m = 3, n = 3, guards = [[1,1]], walls = [[0,1],[1,0],[2,1],[1,2]]
+Output: 4
+Explanation: The unguarded cells are shown in green in the above diagram.
+There are a total of 4 unguarded cells, so we return 4.
+```
+
+**Constraints:** 
+    - `1 <= m, n <= 10^5` 
+    - `2 <= m * n <= 10^5` 
+    - `1 <= guards.length, walls.length <= 5 * 10^4` 
+    - `2 <= guards.length + walls.length <= m * n` 
+    - `guards[i].length == walls[j].length == 2` 
+    - `0 <= row_i, row_j < m` 
+    - `0 <= col_i, col_j < n` 
+    - All the positions in `guards` and `walls` are **unique**.
+"""
+
+import unittest
+from typing import List
+
+class Solution:
+    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        grid = [[''] * n for _ in range(m)]
+        for x, y in guards:
+            grid[x][y] = 'G'
+        for x, y in walls:
+            grid[x][y] = 'W'
+
+        queue = []
+        for x, y in guards:
+            for dx, dy in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
+                if 0 <= x + dx < m \
+                        and 0 <= y + dy < n \
+                        and grid[x + dx][y + dy] != 'W' \
+                        and grid[x + dx][y + dy] != 'G':
+                    queue += [(x + dx, y + dy, dx, dy)]
+
+        while queue:
+            x, y, dx, dy = queue.pop(0)
+            grid[x][y] = '1'
+            if 0 <= x + dx < m \
+                    and 0 <= y + dy < n \
+                    and grid[x + dx][y + dy] != 'W' \
+                    and grid[x + dx][y + dy] != 'G':
+                grid[x + dx][y + dy] = '1'
+                queue += [(x + dx, y + dy, dx, dy)]
+
+        return sum([row.count('') for row in grid])
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.soln_obj = Solution()
+
+    def testcase1(self):
+        self.assertEqual(self.soln_obj.countUnguarded(4, 6, [[0, 0], [1, 1], [2, 3]], [[0, 1], [2, 2], [1, 4]]), 7)
+
+    def testcase2(self):
+        self.assertEqual(self.soln_obj.countUnguarded(3, 3, [[1, 1]], [[0, 1], [1, 0], [2, 1], [1, 2]]), 4)
+
+    def testcase3(self):
+        self.assertEqual(self.soln_obj.countUnguarded(2, 7, [[1, 5], [1, 1], [1, 6], [0, 2]], [[0, 6], [0, 3], [0, 5]]), 1)
+
+    def testcase4(self):
+        self.assertEqual(self.soln_obj.countUnguarded(6, 10, [[0, 6], [2, 2], [2, 5], [1, 2], [4, 9], [2, 9], [5, 6], [4, 6]], [[1, 5]]), 8)
+
+
+if __name__ == '__main__':
+    unittest.main()
